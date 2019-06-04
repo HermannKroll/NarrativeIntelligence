de58d699c35fb822f63edc42cdc45130ae14c732
Johannes Ruthmann <j.ruthmann@online.de>
2019-06-04 10:26:17 +0200

    added index fix for DNorm

--- preprocessing/tagging/dnorm.py
+++ preprocessing/tagging/dnorm.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 from datetime import datetime
 from time import sleep
@@ -8,17 +9,42 @@ from tools import concat, count_documents
 
 
 class DNorm(BaseTagger):
+    def get_document_dict(self, pmid):
+        with open(os.path.join(self.translation_dir, "PMC{}.txt".format(pmid))) as f:
+            content = f.readlines()
+        title = re.sub(r"\d+\|t\| ", "", content[0]).rstrip("\n")
+        abstract = re.sub(r"\d+\|a\| ", "", content[1]).rstrip("\n")
+        return dict(
+            doc=title + abstract,
+            title_len=len(title),
+        )
+
     def finalize(self):
+        documents = dict()
         with open(self.out_file) as f:
             content = f.readlines()
         with open(self.result_file, "w") as f_out:
             for line in content:
                 if line.strip():
-                    new_line = line.split("\t")
-                    new_line.insert(4, "Disease")
-                    f_out.write("\t".join(new_line))
-                else:
-                    f_out.write(line)
+                    new_line = line.strip().split("\t")
+                    # Add type
+                    if len(new_line) == 4 or len(new_line) == 5:
+                        new_line.insert(4, "Disease")
+                    if len(new_line) == 4:
+                        new_line.insert(5, "")
+                    # Read source document
+                    if new_line[0] not in documents:
+                        documents[new_line[0]] = self.get_document_dict(new_line[0])
+                    # Perform indexing check
+                    doc = documents[new_line[0]]["doc"]
+                    if doc[int(new_line[1]):int(new_line[2])] != new_line[3]:
+                        title_len = documents[new_line[0]]["title_len"]
+                        idx_left = int(new_line[1]) + title_len
+                        idx_right = int(new_line[2]) + title_len
+                        new_line[1] = str(idx_left)
+                        new_line[2] = str(idx_right)
+                    # Write result
+                    f_out.write("\t".join(new_line) + "\n")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
