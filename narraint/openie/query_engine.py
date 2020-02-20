b497b1cc1e9a1eadce449d4d84c5bd518efd27e1
HermannKroll <hermann.kroll@gmail.com>
2020-02-20 16:09:57 +0100

    support querying on open ie extractions

---
+++ narraint/openie/query_engine.py
@@ -0,0 +1,123 @@
+import logging
+
+from sqlalchemy.orm import aliased
+from sqlalchemy.sql.expression import Select
+
+from narraint.graph.labeled import LabeledGraph
+from narraint.backend.models import Predication
+from narraint.backend.database import Session
+from narraint.mesh.data import MeSHDB
+from sqlalchemy.dialects import postgresql
+
+
+def __construct_query(session, graph_query, doc_collection):
+    var_names = []
+    var_dict = {}
+    predication_aliases = []
+    for idx, _ in enumerate(graph_query):
+        predication_aliases.append(aliased(Predication, name='P{}'.format(idx)))
+
+    projection_list = []
+    p = predication_aliases[0]
+    projection_list.extend([p.document_id, p.subject_id, p.subject_str, p.subject_type, p.predicate_cleaned,
+                            p.object_id, p.object_str, p.object_type])
+    for p in predication_aliases[1:]:
+        projection_list.extend([p.subject_id, p.subject_str, p.subject_type, p.predicate_cleaned, p.object_id,
+                                p.object_str, p.object_type])
+
+    query = session.query(*projection_list)
+    pred0 = predication_aliases[0]
+    for pred in predication_aliases[1:]:
+        query = query.filter(pred0.document_id == pred.document_id)
+
+    for idx, (s, p, o) in enumerate(graph_query):
+        pred = predication_aliases[idx]
+        query = query.filter(pred.document_collection == doc_collection)
+
+        # variable in pattern
+        if s.startswith('?') or p.startswith('?') or o.startswith('?'):
+            # the idea of the following blocks is as follows:
+            # if x ( here subject, predicate or object) is not a var -> just add a where condition
+            # if x is a variable
+            # check if x already occurred -> yes join both aliased predication together
+            # if x is new, just add it as the last predication of the variable
+            if not s.startswith('?'):
+                query = query.filter(pred.subject_id == s)
+            else:
+                if s not in var_dict:
+                    var_names.append((s, 'subject', idx))
+                    var_dict[s] = (pred, 'subject')
+                else:
+                    last_pred, t = var_dict[s]
+                    if t == 'subject':
+                        query = query.filter(pred.subject_id == last_pred.subject_id)
+                    elif t == 'object':
+                        query = query.filter(pred.subject_id == last_pred.object_id)
+                    else:
+                        ValueError('Variable cannot be used as predicate and subject / object.')
+            if not o.startswith('?'):
+                query = query.filter(pred.object_id == o)
+            else:
+                if o not in var_dict:
+                    var_names.append((o, 'object', idx))
+                    var_dict[o] = (pred, 'object')
+                else:
+                    last_pred, t = var_dict[o]
+                    if t == 'object':
+                        query = query.filter(pred.object_id == last_pred.object_id)
+                    elif t == 'subject':
+                        query = query.filter(pred.object_id == last_pred.subject_id)
+                    else:
+                        raise ValueError('Variable cannot be used as predicate and subject / object.')
+
+            if not p.startswith('?'):
+                query = query.filter(pred.predicate_cleaned == p)
+            else:
+                if p not in var_dict:
+                    var_names.append((p, 'predicate', idx))
+                    var_dict[p] = (pred, 'predicate')
+                else:
+                    last_pred, t = var_dict[p]
+                    if t == 'predicate':
+                        query = query.filter(pred.predicate_cleaned == last_pred.predicate_cleaned)
+                    else:
+                        raise ValueError('Variable cannot be used as predicate and subject / object.')
+        else:
+            query = query.filter(pred.subject_id == s, pred.object_id == o, pred.predicate_cleaned == p)
+
+    return query, var_names
+
+
+def query_with_graph_query(graph_query, doc_collection='PMC'):
+    if len(graph_query) == 0:
+        raise ValueError('graph query must contain at least one fact')
+
+    session = Session.get()
+    query, var_info = __construct_query(session, graph_query, doc_collection)
+
+    print(str(query.statement.compile(dialect=postgresql.dialect())))
+    pmids, titles, var_subs, var_names = [], [], [], []
+    for v, _, _ in var_info:
+        var_names.append(v)
+    for r in session.execute(query):
+        pmids.append(r[0])
+        titles.append("test") # r[1])
+        # extract var substitutions for pmid
+        var_sub = {}
+        for v, t, pred_pos in var_info:
+            offset = 1 + pred_pos * 5
+            if t == 'subject':
+                var_sub[v] = '{} ({} : {})'.format(r[offset], r[offset+1], r[offset+2])
+            elif t == 'object':
+                var_sub[v] = '{} ({} : {})'.format(r[offset+4], r[offset+5], r[offset+6])
+            elif t == 'predicate':
+                var_sub[v] = '{}'.format(r[offset+3])
+            else:
+                raise ValueError('Unknown position in query projection')
+        var_subs.append(var_sub)
+
+    logging.info("{} hits: {}".format(len(pmids), pmids))
+    return pmids, titles, var_subs, var_names
+
+
+
