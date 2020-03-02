bac91cfe9d9fded35ffb48d6cd82ee9897da98bb
J. Pirklbauer <jan.pirklbauer@online.de>
2020-03-02 20:18:59 +0100

    workload distribution and subdir creation + Implemented distribute_workload to split input directory into number_of_workers input directories. still somewhat flawed but it's at least something. + Creating root and input subdirectories for multiple threads

--- narraint/preprocessing/preprocess.py
+++ narraint/preprocessing/preprocess.py
@@ -2,7 +2,9 @@ import logging
 import os
 import tempfile
 from argparse import ArgumentParser
+from shutil import copy
 from typing import List
+import multiprocessing
 
 from narraint.backend import enttypes
 from narraint.backend.database import Session
@@ -21,7 +23,9 @@ from narraint.preprocessing.tagging.gnormplus import GNormPlus
 from narraint.preprocessing.tagging.taggerone import TaggerOne
 from narraint.preprocessing.tagging.tmchem import TMChem
 from narraint.pubtator.convert import PMCConverter
+from narraint.pubtator.count import count_documents
 from narraint.pubtator.document import get_document_id, DocumentError
+from narraint.pubtator.split import split
 
 LOGGING_FORMAT = '%(asctime)s %(levelname)s %(threadName)s %(module)s:%(lineno)d %(message)s'
 
@@ -85,6 +89,64 @@ def get_untagged_doc_ids_by_ent_type(collection, target_ids, ent_type, tagger_cl
         ))
     missing_ids = target_ids.difference(present_ids)
     return missing_ids
+
+def create_parallel_dirs(root, number, prefix, *subdirs):
+    """
+    Creates number identical subdirectories named <prefix><index> containing subdirectories specified with the names
+    given in *subdirs.
+    :param root: The directory where the subdirectories are to be created
+    :param number: The number of identical subdirs to be created
+    :param prefix: The prefix in the name of the subdirs
+    :param subdirs: The subdirectories to be contained by each identical directory
+    """
+    for n in range(number):
+        indexed_dir = os.path.join(root, f"{prefix}{n}")
+        if not os.path.exists(indexed_dir):
+            os.makedirs(indexed_dir)
+        for name in subdirs:
+            subdir_path = os.path.join(root, f"{prefix}{n}", name)
+            if not os.path.exists(subdir_path):
+                os.makedirs(subdir_path)
+
+def distribute_workload(input_dir, output_root, workers_number:int, subdirs_name="batch", ):
+    """
+    Takes an input directory filled with files, each containing one or multiple pubtator documents. Then creates
+    workers_number subdirectories in output_root and distributes the documents equally among them.
+    The files in input_dir will be copied.
+    :param input_dir: dictionary containing the files to be distributed (single pubtator document or multiple in one)
+    :param output_root: path where the the subdirectories for every worker will be created
+    :param workers_number: the number of workers to distribute the workload on
+    :param subdirs_name: the prefix to the batch subdirs
+    """
+    #create subdirectories
+    tmp_path = os.path.join(output_root, "tmp")
+    os.makedirs(tmp_path)
+    create_parallel_dirs(output_root, workers_number, subdirs_name)
+    paths = (os.path.join(input_dir, file) for file in os.listdir(input_dir))
+    file_sizes = {path: os.path.getsize(path) for path in paths if os.path.isfile(path)}
+    total_workload = sum(file_sizes.values())
+    workload_per_worker = total_workload // (workers_number-1)
+
+    current_worker_id=0
+    current_worker_workload=0
+    for file, file_size in file_sizes.items():
+        #TODO: check after adding -> too much workload, checking before adding -> leftover workload. to be fixed
+        if file_size < workload_per_worker:
+            copy(file, os.path.join(output_root,f"{subdirs_name}{current_worker_id}"))
+            current_worker_workload += file_size
+            if current_worker_workload > workload_per_worker:
+                current_worker_id = (current_worker_id+1)%workers_number
+                current_worker_workload=0
+        else:
+            avg_size_per_doc = file_size//count_documents(file)
+            batch_size = workload_per_worker//avg_size_per_doc
+            split(file,tmp_path,batch_size)
+            current_worker_id = (current_worker_id + 1) % workers_number
+            for batch in os.listdir(tmp_path):
+                batch = os.path.join(tmp_path,batch)
+                os.rename(batch, os.path.join(output_root,f"{subdirs_name}{current_worker_id}",os.path.basename(batch)))
+                current_worker_id = (current_worker_id + 1) % workers_number
+            current_worker_workload=0
 
 
 def preprocess(collection, root_dir, input_dir, log_dir, logger, output_filename, conf, *tag_types,
@@ -178,7 +240,7 @@ def main():
     group_settings.add_argument("--workdir", default=None)
     group_settings.add_argument("--skip-load", action='store_true',
                                 help="Skip bulk load of documents on start (expert setting)")
-    group_settings.add_argument("--workers",default=1, help="Number of processes for parallelized preprocessing")
+    group_settings.add_argument("-w", "--workers",default=1, help="Number of processes for parallelized preprocessing")
 
     parser.add_argument("input", help="Directory with PubTator files "
                                       "(can be a file if --ids is set or a directory if --resume is set)",
@@ -216,6 +278,18 @@ def main():
         translator.convert_bulk(files, in_dir, pmcid2pmid, error_file)
     elif args.ids:
         raise logger.exception("Providing an ID set is only supported for PMC collection")
+
+    if not int(args.workers) == 1:
+        logger.info('splitting up workload for multiple threads')
+        distribute_workload(in_dir,os.path.join(root_dir,"inputDirs"),int(args.workers))
+        create_parallel_dirs(root_dir,int(args.workers),"worker", "logs")
+        #processes = []
+        #for n in range(int(args.workers)):
+            #sublogger = init_preprocess_logger(os.path.join(root_dir,"log","preprocessing.log"), args.loglevel.upper())
+            #args = (args.corpus, os.joi)
+            #processes.append(multiprocessing.Process(target = preprocess()))
+
+
 
     # Add documents to database
     if args.skip_load:
