cf29aa7ba8ecad84ba6404f9f59b2c8ddcf3967b
HermannKroll <hermann.kroll@gmail.com>
2020-03-03 11:18:33 +0100

    add index for canonicalized predicates

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -2,5 +2,6 @@ CREATE INDEX pred_subject_id_idx ON PREDICATION(subject_id);
 CREATE INDEX pred_subject_type_idx ON PREDICATION(subject_type);
 CREATE INDEX pred_predicate_idx ON PREDICATION(predicate);
 CREATE INDEX pred_predicate_cleaned_idx ON PREDICATION(predicate_cleaned);
+CREATE INDEX pred_predicate_canonicalized ON PREDICATION(predicate_canonicalized);
 CREATE INDEX pred_object_id_idx ON PREDICATION(object_id);
 CREATE INDEX pred_object_type_idx ON PREDICATION(object_type);
