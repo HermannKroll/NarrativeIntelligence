62a9f616ec50ea1bfc3d0e3861a17092efe78b8a
HermannKroll <hermann.kroll@gmail.com>
2020-03-16 11:48:51 +0100

    supports id file as an input + logging

--- narraint/backend/export.py
+++ narraint/backend/export.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import os
 
 from narraint.backend import enttypes
 from narraint.backend.database import Session
@@ -8,10 +9,13 @@ from narraint.backend.models import Document, Tag
 
 
 def export(out_fn, tag_types, document_ids=None, collection=None, content=True):
+    logging.info("beginning export...")
     if document_ids is None:
         document_ids = []
-    session = Session.get()
+    else:
+        logging.info('using {} ids for a filter condition'.format(len(document_ids)))
 
+    session = Session.get()
     if content and tag_types:
         query = session.query(Document, Tag)
         if collection:
@@ -57,31 +61,45 @@ def export(out_fn, tag_types, document_ids=None, collection=None, content=True):
         with open(out_fn, "w") as f:
             for row in results:
                 f.write(Tag.create_pubtator(row[6], row[2], row[3], row[5], row[1], row[4]))
-    print("Results written to {}".format(out_fn))
+    logging.info("Results written to {}".format(out_fn))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("output")
-    parser.add_argument("ids", nargs="*", metavar="DOC_ID")
+    parser.add_argument("--ids", nargs="*", metavar="DOC_ID")
+    parser.add_argument("--idfile", help='file containing document ids (one id per line)')
     parser.add_argument("-c", "--collection", help="Collection(s)", default=None)
     parser.add_argument("-d", "--document", action="store_true", help="Export content of document")
     parser.add_argument("-t", "--tag", choices=TAG_TYPE_MAPPING.keys(), nargs="+")
-    parser.add_argument("--log", action="store_true")
+    parser.add_argument("--sqllog", action="store_true", help='logs sql commands')
     args = parser.parse_args()
 
     if not (args.tag or args.document):
         parser.error('No action requested, add -d or -t')
 
-    if args.log:
-        logging.basicConfig()
+    if args.ids and args.idfile:
+        parser.error('Does not support a list of ids and an ids file')
+
+    logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                        datefmt='%Y-%m-%d:%H:%M:%S',
+                        level=logging.DEBUG)
+    if args.sqllog:
         logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
     tag_types = []
     if args.tag:
         tag_types = enttypes.ALL if "A" in args.tag else [TAG_TYPE_MAPPING[x] for x in args.tag]
 
-    document_ids = [int(x) for x in args.ids]
+    if args.ids:
+        document_ids = [int(x) for x in args.ids]
+    elif args.idfile:
+        logging.info('reading id file: {}'.format(args.idfile))
+        with open(args.idfile, 'r') as f:
+            document_ids = list(set([int(line.strip()) for line in f]))
+        logging.info('{} ids retrieved from id file..'.format(len(document_ids)))
+    else:
+        document_ids = None
 
     export(args.output, tag_types, document_ids, collection=args.collection, content=args.document)
 
