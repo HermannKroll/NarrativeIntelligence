7ab52f92a5b53fb92c266aaacd6b8831ab1ce044
HermannKroll <hermann.kroll@gmail.com>
2020-03-30 11:30:26 +0200

    add indexes to tagging db to increase join performance for tag table

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -7,3 +7,6 @@ CREATE INDEX pred_predicate_cleaned_idx ON PREDICATION(predicate_cleaned);
 CREATE INDEX pred_predicate_canonicalized ON PREDICATION(predicate_canonicalized);
 CREATE INDEX pred_object_id_idx ON PREDICATION(object_id);
 CREATE INDEX pred_object_type_idx ON PREDICATION(object_type);
+
+CREATE INDEX tag_document_id_idx ON TAG(document_id);
+CREATE INDEX tag_document_collection_idx ON TAG(document_collection);
