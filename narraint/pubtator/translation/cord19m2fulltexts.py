be80e4ff0dfb854abd79383a1f91e6009efb3b76
HermannKroll <hermann.kroll@gmail.com>
2020-04-29 08:44:51 +0200

    split fulltexts into smaller sections in pubtator format

--- narraint/pubtator/translation/cord19m2fulltexts.py
+++ narraint/pubtator/translation/cord19m2fulltexts.py
@@ -6,7 +6,8 @@ from datetime import datetime
 from narraint.backend.models import Document
 from narraint.progress import print_progress_with_eta
 
-UNIQUE_ID_START = 1
+UNIQUE_ID_START = 100000
+NEXT_DOCUMENT_ID_OFFSET = 100000
 
 
 class FileReader:
@@ -16,7 +17,7 @@ class FileReader:
             self.paper_id = content['paper_id']
             self.title = ""
             self.abstract = []
-            self.body_text = []
+            self.body_texts = []
             # Title
             self.title = content['metadata']["title"]
             # Abstract
@@ -25,12 +26,8 @@ class FileReader:
                     self.abstract.append(entry['text'])
             # Body text
             for entry in content['body_text']:
-                self.body_text.append(entry['text'])
+                self.body_texts.append(entry['text'])
             self.abstract = '\n'.join(self.abstract)
-            self.body_text = '\n'.join(self.body_text)
-
-    def to_pubtator(self, document_id):
-        return Document.create_pubtator(document_id, self.title, self.abstract + self.body_text)
 
     def __repr__(self):
         return f'{self.paper_id}: {self.abstract[:200]}... {self.body_text[:200]}...'
@@ -52,9 +49,19 @@ def main():
         for idx, json_file in enumerate(all_json):
             try:
                 file = FileReader(json_file)
-                doc_id = UNIQUE_ID_START + idx
-                content = file.to_pubtator(doc_id)
-                f.write(content + '\n')
+                doc_id = UNIQUE_ID_START + (NEXT_DOCUMENT_ID_OFFSET * idx)
+                # export title + abstract
+                if len(file.abstract) > 0:
+                    content = Document.create_pubtator(doc_id, file.title, file.abstract)
+                    f.write(content + '\n')
+
+                for body_text_id, body_text in enumerate(file.body_texts):
+                    if body_text_id > 99999:
+                        raise ValueError('Overflow body id - increase id range for document bodies')
+                    artificial_doc_id = doc_id + body_text_id + 1
+                    content = Document.create_pubtator(artificial_doc_id, "Section", body_text)
+                    f.write(content + '\n')
+
                 f_info.write('\n{}\t{}'.format(doc_id, file.paper_id))
                 print_progress_with_eta('converting', idx, len(all_json), start_time)
             except KeyError:
