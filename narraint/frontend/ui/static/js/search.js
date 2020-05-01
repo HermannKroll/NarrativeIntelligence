076fb169da16805957c50eb784c19bec781a814b
HermannKroll <hermann.kroll@gmail.com>
2020-05-01 19:43:10 +0200

    remove old stuff

--- narraint/frontend/ui/static/js/search.js
+++ narraint/frontend/ui/static/js/search.js
@@ -258,22 +258,6 @@ $(document).ready(function () {
     });
 });
 
-function add_collapsable_events() {
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    }
-}
-
 const search = (event) => {
     event.preventDefault();
     let query = $('#id_keywords').val();
