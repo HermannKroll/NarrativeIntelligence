9f7bf808846090a73d18a8f7137aa96d0e5670bc
Johannes Ruthmann <j.ruthmann@online.de>
2020-05-11 19:12:25 +0200

    added filtering

--- narraint/analysis/pubmed_medline.py
+++ narraint/analysis/pubmed_medline.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 from lxml import etree
 
+from narraint.backend.database import Session
+from narraint.backend.models import DocProcessedByOpenIE
 from narraint.config import MEDLINE_BASELINE_INDEX
 from narraint.progress import print_progress_with_eta
 
@@ -23,13 +25,14 @@ class PubMedMEDLINE:
             return set()
 
 
-def load_file(filename):
+def load_file(filename, db_pmids):
     with open(filename) as f:
         tree = etree.parse(f)
 
     pmid_to_descs = {}
     for article in tree.iterfind("PubmedArticle"):
         descs = set()
+
         # Get PMID
         pmids = article.findall("./MedlineCitation/PMID")
         if len(pmids) > 1:
@@ -37,6 +40,9 @@ def load_file(filename):
             continue  # BAD
         else:
             pmid = int(pmids[0].text)
+
+        if pmid not in db_pmids:
+            continue  # BAD
 
         # Parse Mesh heading
         for mh in article.iterfind("./MedlineCitation/MeshHeadingList/MeshHeading"):
@@ -62,14 +68,14 @@ def load_file(filename):
     return pmid_to_descs
 
 
-def load_files(directory):
+def load_files(directory, db_pmids):
     desc_to_pmids = {}
 
     files = [os.path.join(directory, fn) for fn in os.listdir(directory) if fn.endswith(".xml")]
 
     start = datetime.now()
     for idx, fn in enumerate(files):
-        pmid_to_descs = load_file(fn)
+        pmid_to_descs = load_file(fn, db_pmids)
         for pmid, descs in pmid_to_descs.items():
             for desc in descs:
                 if desc not in desc_to_pmids:
@@ -84,15 +90,24 @@ def load_files(directory):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--build-index", "-i", help="Build index", action="store_true")
-    parser.add_argument("directory", help="Directory containing XML files", metavar="DIR")
+    parser.add_argument("--build-index", "-i", help="Build index (default: {})".format(MEDLINE_BASELINE_INDEX),
+                        const=MEDLINE_BASELINE_INDEX, metavar="INDEX_FILE", nargs="?")
+    parser.add_argument("--dir", "-d", help="Directory containing XML files", metavar="DIR", required=True)
     args = parser.parse_args()
 
+    # Query database
+    session = Session.get()
+    query = session.query(DocProcessedByOpenIE.document_id).filter(DocProcessedByOpenIE.document_collection == "PubMed")
+    results = session.execute(query)
+    db_pmids = set(x[0] for x in results)
+    print("{} documents processed by OpenID".format(len(db_pmids)))
+
+    # Build index
     if args.build_index:
-        desc_to_pmids = load_files(args.directory)
+        desc_to_pmids = load_files(args.dir, db_pmids)
 
         print("\nPickling index ...")
-        with open(MEDLINE_BASELINE_INDEX, "wb")as f:
+        with open(args.build_index, "wb")as f:
             pickle.dump(desc_to_pmids, f)
     else:
         print("Nothing to do")
