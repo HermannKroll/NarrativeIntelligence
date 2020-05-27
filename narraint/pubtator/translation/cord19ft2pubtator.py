3494e0e6179d02f459fcb4b3ef30315383caadbd
J. Pirklbauer <jan.pirklbauer@online.de>
2020-05-27 14:38:03 +0200

    Issue #40 - read_meta now supports multi-values

--- narraint/pubtator/translation/cord19ft2pubtator.py
+++ narraint/pubtator/translation/cord19ft2pubtator.py
@@ -4,6 +4,10 @@ import csv
 from argparse import ArgumentParser
 from datetime import datetime
 
+from sqlalchemy.dialects.postgresql import insert
+
+from narraint.backend import models
+from narraint.backend.database import Session
 from narraint.backend.models import Document
 from narraint.progress import print_progress_with_eta
 
@@ -36,52 +40,69 @@ class FileReader:
 
 def main():
     parser = ArgumentParser(description="Collect and convert PMC files from a list of pmc-ids")
-    parser.add_argument("input", help="Root Directory of Cord19m2 data setw")
+    parser.add_argument("input", help="Root Directory of Cord19m2 data set")
     parser.add_argument("metadata", help="Path to metadata.csv file")
     parser.add_argument("output", help="Output of the resulting Pubtator file")
     args = parser.parse_args()
 
-    translate(args.input, args.output)
+    translator = Translator(args.input, args.output, args.metadata)
+    dict_list = read_metadata(args.metadata)
 
 
-"""
-recursively searches for all .json files in input_dir, sanitizes them and converts them to a pubtator format. 
-:parm input_dir: directory containing the json files
-:param output_dir: an (existing) file path to write the pubtator output to
-"""
-def translate(input_dir, output_file):
-    print('searching all json files in {}'.format(input_dir))
-    all_json = glob.glob(f'{input_dir}/**/*.json', recursive=True)
-    print('{} json files found'.format(len(all_json)))
-    start_time = datetime.now()
-    with open(output_file, 'w') as f, open(output_file + '.info.csv', 'w') as f_info:
-        f_info.write('document_id\tpaper_id')
-        for idx, json_file in enumerate(all_json):
-            try:
-                file = FileReader(json_file)
-                doc_id = UNIQUE_ID_START + (NEXT_DOCUMENT_ID_OFFSET * idx)
-                # export title + abstract
-                if len(file.abstract) > 0:
-                    content = Document.create_pubtator(doc_id, file.title, file.abstract)
-                    f.write(content + '\n')
+class Translator:
+    def __init__(self, input_dir, output_dir, metadata_file):
+        self.metadata_dict = read_metadata(metadata_file)
+        self.session = Session.get()
 
-                for body_text_id, body_text in enumerate(file.body_texts):
-                    if body_text_id >= NEXT_DOCUMENT_ID_OFFSET:
-                        raise ValueError('Overflow body id - increase id range for document bodies')
-                    artificial_doc_id = doc_id + body_text_id + 1
-                    content = Document.create_pubtator(artificial_doc_id, "Section", body_text)
-                    f.write(content + '\n')
+    """
+    recursively searches for all .json files in input_dir, sanitizes them and converts them to a pubtator format. 
+    :parm input_dir: directory containing the json files
+    :param output_dir: an (existing) file path to write the pubtator output to
+    """
+    def translate(input_dir, output_file):
+        print('searching all json files in {}'.format(input_dir))
+        all_json = glob.glob(f'{input_dir}/**/*.json', recursive=True)
+        print('{} json files found'.format(len(all_json)))
+        start_time = datetime.now()
+        with open(output_file, 'w') as f, open(output_file + '.info.csv', 'w') as f_info:
+            f_info.write('document_id\tpaper_id')
+            for idx, json_file in enumerate(all_json):
+                try:
+                    file = FileReader(json_file)
+                    doc_id = UNIQUE_ID_START + (NEXT_DOCUMENT_ID_OFFSET * idx)
+                    # export title + abstract
+                    if len(file.abstract) > 0:
+                        content = Document.create_pubtator(doc_id, file.title, file.abstract)
+                        f.write(content + '\n')
 
-                f_info.write('\n{}\t{}'.format(doc_id, file.paper_id))
-                print_progress_with_eta('converting', idx, len(all_json), start_time)
-            except KeyError:
-                print('skip: {}'.format(json_file))
+                    for body_text_id, body_text in enumerate(file.body_texts):
+                        if body_text_id >= NEXT_DOCUMENT_ID_OFFSET:
+                            raise ValueError('Overflow body id - increase id range for document bodies')
+                        artificial_doc_id = doc_id + body_text_id + 1
+                        content = Document.create_pubtator(artificial_doc_id, "Section", body_text)
+                        f.write(content + '\n')
+
+                    f_info.write('\n{}\t{}'.format(doc_id, file.paper_id))
+                    print_progress_with_eta('converting', idx, len(all_json), start_time)
+                except KeyError:
+                    print('skip: {}'.format(json_file))
+
+
+    def insert_translation(art_id, sha):
+
+        insert_query = insert(models.Cord19Translation).values(
+
+        )
 
 
 def read_metadata(metadata_file):
-    with open(metadata_file, 'rb') as f:
+    with open(metadata_file, 'r') as f:
         reader = csv.DictReader(f)
-        dict_list = [d for d in reader]
+        dict_list = []
+        for d in reader:
+            for k in d.keys():
+                d[k] = d[k].split(";") # Iterpret ; splitted values as list
+            dict_list.append(d)
         return dict_list
 
 if __name__ == "__main__":
