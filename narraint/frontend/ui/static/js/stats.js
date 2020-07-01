76b6a39838e90ee39fb9deaf02777d7d319df399
Morris <morris.kunz@tu-bs.de>
2020-07-01 16:43:03 +0200

    time to rebase

--- narraint/frontend/ui/static/js/stats.js
+++ narraint/frontend/ui/static/js/stats.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-    console.log("CP1");
     $("#stats_form").submit(stats);
-})
+});
 
 const stats = (event) => {
-
-}
+    console.log("CP1");
+    event.preventDefault();
+    let request = $.ajax({
+        url: stats_url
+    });
+    request.done(function (response) {
+        console.log("Success: " + response);
+    });
+    request.fail(function (result) {
+        console.log("Fail: " + result);
+    });
+};
