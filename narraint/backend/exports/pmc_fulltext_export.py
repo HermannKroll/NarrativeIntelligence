934810d4dee69f68a163200f4bce04b7f405b168
HermannKroll <hermann.kroll@gmail.com>
2020-09-02 16:52:48 +0200

    export document iff pmc documents were found + bug fixes

--- narraint/backend/exports/pmc_fulltext_export.py
+++ narraint/backend/exports/pmc_fulltext_export.py
@@ -6,6 +6,7 @@ import tempfile
 from narraint.config import PREPROCESS_CONFIG
 from narraint.preprocessing.collect import PMCCollector
 from narraint.preprocessing.config import Config
+from narraint.preprocessing.convertids import load_pmcids_to_pmid_index
 from narraint.pubtator.enrich_docs_with_tags import enrich_pubtator_documents_with_database_tags
 from narraint.pubtator.translation.pmc import PMCConverter
 
@@ -25,14 +26,24 @@ def export_pmc_fulltexts_with_tags(out_fn, tag_types, doc_ids):
     tmp_dir_pmc_files = os.path.join(tmp_dir, 'pmc')
     os.mkdir(tmp_dir_pmc_files)
     logging.info('working in temp directory: {}'.format(tmp_dir))
+    logging.info('Load PMCID to PMID translation file')
+    pmcid2pmid = load_pmcids_to_pmid_index(conf.pmcid2pmid)
+    pmid2pmcid = {v: k for k, v in pmcid2pmid.items()}
+    pmcid_doc_ids = set()
+    for doc_id in doc_ids:
+        if doc_id in pmid2pmcid:
+            pmcid_doc_ids.add('PMC{}'.format(pmid2pmcid[doc_id]))
+    logging.info('{} of {} have a correct PMCID...'.format(len(pmcid_doc_ids), len(doc_ids)))
+    if pmcid_doc_ids:
+        error_file = os.path.join(tmp_dir, "conversion_errors.txt")
+        collector = PMCCollector(conf.pmc_dir)
+        files = collector.collect(pmcid_doc_ids)
+        if files:
+            logging.info('{} matching nxml files found ({} of {})'.format(len(files), len(files), len(doc_ids)))
+            translator = PMCConverter()
+            translator.convert_bulk(files, tmp_dir_pmc_files, pmcid2pmid, error_file)
 
-    error_file = os.path.join(tmp_dir, "conversion_errors.txt")
-    collector = PMCCollector(conf.pmc_dir)
-    files = collector.collect(doc_ids)
-    translator = PMCConverter()
-    translator.convert_bulk(files, tmp_dir_pmc_files, {}, error_file)
-
-    enrich_pubtator_documents_with_database_tags(tmp_dir_pmc_files, out_fn, 'PMC', tag_types)
+            enrich_pubtator_documents_with_database_tags(tmp_dir_pmc_files, out_fn, 'PMC', tag_types)
     shutil.rmtree(tmp_dir)
-    logging.info('export to {} finished.temp directory {} removed'.format(out_fn, tmp_dir))
+    logging.info('export to {} finished -- temp directory {} removed'.format(out_fn, tmp_dir))
 
