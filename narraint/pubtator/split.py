9834efa7476bb2a4ac4434c53b6a0554fe08857e
HermannKroll <hermann.kroll@gmail.com>
2020-09-03 09:51:56 +0200

    enhance split by a document prefix

--- narraint/pubtator/split.py
+++ narraint/pubtator/split.py
@@ -5,17 +5,18 @@ from narraint.pubtator.regex import DOCUMENT_ID
 from narraint.pubtator.extract import read_pubtator_documents
 
 
-def write_content(content, out_dir):
+def write_content(content, out_dir, document_prefix=""):
     doc_ids = DOCUMENT_ID.findall(content)
     if doc_ids:
-        filename = f"{doc_ids[0]}.txt" if doc_ids[0] == doc_ids[-1] else f"{doc_ids[0]}-{doc_ids[-1]}.txt"
+        filename = f"{document_prefix}{doc_ids[0]}.txt" if doc_ids[0] == doc_ids[-1] \
+            else f"{document_prefix}{doc_ids[0]}-{doc_ids[-1]}.txt"
         with open(os.path.join(out_dir, filename), "w") as f:
             f.write(content + '\n')  # pubtator files must be terminated by an \n
     else:
         raise ValueError("No ID for {}".format(content))
 
 
-def split(filename, out_dir, batch_size=1, logger=None):
+def split(filename, out_dir, batch_size=1, logger=None, document_prefix=""):
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
     docs_in_batch = 0
@@ -26,7 +27,7 @@ def split(filename, out_dir, batch_size=1, logger=None):
         doc_group_content.append(doc_content)
         # FIXME: Ensure that last batch possibly being smaller than the batch size is written anyway
         if docs_in_batch >= batch_size:
-            write_content('\n'.join(doc_group_content), out_dir)
+            write_content('\n'.join(doc_group_content), out_dir, document_prefix)
             doc_group_content = []
             docs_in_batch = 0
     if logger: logger.debug('finished')
