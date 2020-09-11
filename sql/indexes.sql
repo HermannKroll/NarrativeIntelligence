be407226610c183d6db003a3be18b29c646794b8
HermannKroll <hermann.kroll@gmail.com>
2020-09-11 19:03:34 +0200

    unique constraint is created when the table is created

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,12 +1,8 @@
-CREATE UNIQUE INDEX unique_doc_s_p_o_sent_idx ON PREDICATION(document_id, document_collection, subject_id, subject_type,
- predicate, object_id, object_type, extraction_type, md5(sentence::text));
-
 CREATE INDEX pred_document_id_idx ON PREDICATION(document_id);
 CREATE INDEX pred_document_col_idx ON PREDICATION(document_collection);
 CREATE INDEX pred_subject_id_idx ON PREDICATION(subject_id);
 CREATE INDEX pred_subject_type_idx ON PREDICATION(subject_type);
 CREATE INDEX pred_predicate_idx ON PREDICATION(predicate);
-CREATE INDEX pred_predicate_cleaned_idx ON PREDICATION(predicate_cleaned);
 CREATE INDEX pred_predicate_canonicalized ON PREDICATION(predicate_canonicalized);
 CREATE INDEX pred_object_id_idx ON PREDICATION(object_id);
 CREATE INDEX pred_object_type_idx ON PREDICATION(object_type);
