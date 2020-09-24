24f2fe014d48fa154ce93393934d46268433625c
HermannKroll <hermann.kroll@gmail.com>
2020-09-24 13:55:19 +0200

    remove all predications with chemicals consisting only of 2 characters

--- sql/clean_Predication.sql
+++ sql/clean_Predication.sql
@@ -7,3 +7,4 @@ DELETE FROM PREDICATION WHERE (subject_str like '%_gely' and subject_type = 'Dos
 
 -- Chemicals with only a single character are mostly wrong tagged
 DELETE FROM PREDICATION WHERE (subject_str like '_' and subject_type = 'Chemical') or (object_str like '_' and object_type = 'Chemical');
+DELETE FROM PREDICATION WHERE (subject_str like '__' and subject_type = 'Chemical') or (object_str like '__' and object_type = 'Chemical');
