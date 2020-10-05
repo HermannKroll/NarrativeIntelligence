ea0f498de1fd119f492f12d961e8f3e3ea633dab
HermannKroll <hermann.kroll@gmail.com>
2020-10-05 12:33:36 +0200

    adjust dosage form rules and fix mirroring

--- narraint/extraction/apply_rules.py
+++ narraint/extraction/apply_rules.py
@@ -14,7 +14,7 @@ from narraint.progress import print_progress_with_eta
 
 DOSAGE_FORM_PREDICATE = "administered"
 ASSOCIATED_PREDICATE = "associated"
-SYMMETRIC_PREDICATES = {DOSAGE_FORM_PREDICATE, ASSOCIATED_PREDICATE}
+SYMMETRIC_PREDICATES = {DOSAGE_FORM_PREDICATE}  # , ASSOCIATED_PREDICATE}
 
 
 def dosage_form_rule():
@@ -25,81 +25,21 @@ def dosage_form_rule():
     logging.info('Applying DosageForm rule...')
     session = Session.get()
 
-    logging.info('Updating predicate to "{}" for (DosageForm, Chemical) pairs'.format(DOSAGE_FORM))
+    logging.info(
+        'Updating predicate to "{}" for (DosageForm, [Chemical, Disease, Species]) pairs'.format(DOSAGE_FORM_PREDICATE))
     stmt_1 = update(Predication).where(and_(Predication.subject_type == DOSAGE_FORM,
-                                            Predication.object_type == CHEMICAL)). \
+                                            Predication.object_type.in_([CHEMICAL, DISEASE, SPECIES]))). \
         values(predicate_canonicalized=DOSAGE_FORM_PREDICATE)
     session.execute(stmt_1)
     session.commit()
 
-    logging.info('Updating predicate to "{}" for (Chemical, DosageForm) pairs'.format(DOSAGE_FORM))
-    stmt_2 = update(Predication).where(and_(Predication.subject_type == CHEMICAL,
-                                            Predication.object_type == DOSAGE_FORM)). \
+    logging.info(
+        'Updating predicate to "{}" for ([Chemical, Disease, Species], DosageForm) pairs'.format(DOSAGE_FORM_PREDICATE))
+    stmt_2 = update(Predication).where(and_(Predication.object_type == DOSAGE_FORM,
+                                            Predication.subject_type.in_([CHEMICAL, DISEASE, SPECIES]))). \
         values(predicate_canonicalized=DOSAGE_FORM_PREDICATE)
     session.execute(stmt_2)
     session.commit()
-
-    logging.info('Updating predicate to "{}" for (DosageForm, Disease) pairs'.format(DOSAGE_FORM))
-    stmt_1 = update(Predication).where(and_(Predication.subject_type == DOSAGE_FORM,
-                                            Predication.object_type == DISEASE)). \
-        values(predicate_canonicalized=DOSAGE_FORM_PREDICATE)
-    session.execute(stmt_1)
-    session.commit()
-
-    logging.info('Updating predicate to "{}" for (Disease, DosageForm) pairs'.format(DOSAGE_FORM))
-    stmt_2 = update(Predication).where(and_(Predication.subject_type == DISEASE,
-                                            Predication.object_type == DOSAGE_FORM)). \
-        values(predicate_canonicalized=DOSAGE_FORM_PREDICATE)
-    session.execute(stmt_2)
-    session.commit()
-
-
-def mirror_symmetric_predicates():
-    """
-    Some predicates are symmetric - these predicates will be mirrored in the database
-    :return: None
-    """
-    session = Session.get()
-    logging.info('Mirroring symmetric predicates...')
-    for idx_pred, pred_to_mirror in enumerate(SYMMETRIC_PREDICATES):
-        start_time = datetime.now()
-        logging.info('Mirroring predicate: {}'.format(pred_to_mirror))
-        q = session.query(Predication)
-        q = q.filter(Predication.predicate_canonicalized == pred_to_mirror)
-        count_mirrored = 0
-        for r in session.execute(q):
-            p = PredicationResult(*r)
-
-            try:
-                insert_pred = insert(Predication).values(
-                    document_id=p.document_id,
-                    document_collection=p.document_collection,
-                    subject_id=p.object_id,
-                    subject_str=p.object_str,
-                    subject_type=p.object_type,
-                    predicate=p.predicate,
-                    predicate_canonicalized=p.predicate_canonicalized,
-                    object_id=p.subject_id,
-                    object_str=p.subject_str,
-                    object_type=p.subject_type,
-                    confidence=p.confidence,
-                    sentence_id=p.sentence_id,
-                    extraction_type=p.extraction_type
-                )
-                session.execute(insert_pred)
-                session.commit()
-            except IntegrityError:
-                logging.warning('Skip duplicated fact: ({}, {}, {}, {}, {}, {})'.
-                                format(p.document_id, p.document_collection, p.subject_id, p.predicate, p.object_id,
-                                       p.sentence_id))
-
-                session.rollback()
-                session.commit()
-            count_mirrored += 1
-        logging.info('{} facts mirrored for predicate: {} (in {}s)'.format(count_mirrored, pred_to_mirror,
-                                                                           datetime.now() - start_time))
-
-    logging.info('Mirroring predicates finished')
 
 
 def clean_extractions_in_database():
@@ -109,15 +49,32 @@ def clean_extractions_in_database():
     """
     session = Session.get()
 
-    logging.info('Cleaning administered (DosageForm -> Chemical, Disease Species)...')
+    logging.info('Cleaning administered (not DosageForm -> not DosageForm)...')
     q_administered = update(Predication).where(and_(Predication.predicate_canonicalized == 'administered',
-                                                    or_(Predication.subject_type != DOSAGE_FORM,
-                                                        Predication.object_type.notin_([SPECIES, DISEASE, CHEMICAL]))))\
+                                                    and_(Predication.subject_type != DOSAGE_FORM,
+                                                         Predication.object_type != DOSAGE_FORM))) \
         .values(predicate_canonicalized=None)
     session.execute(q_administered)
     session.commit()
 
-    logging.info('Cleaning induces (Chemical -> Chemical / Disease)')
+    logging.info('Cleaning administered (DosageForm -> [Chemical, Disease Species])...')
+    q_administered = update(Predication).where(and_(Predication.predicate_canonicalized == 'administered',
+                                                    and_(Predication.subject_type == DOSAGE_FORM,
+                                                         Predication.object_type.notin_([SPECIES, DISEASE, CHEMICAL]))))\
+        .values(predicate_canonicalized=None)
+    session.execute(q_administered)
+    session.commit()
+
+    logging.info('Cleaning administered ([Chemical, Disease Species] -> DosageForm)...')
+    q_administered = update(Predication).where(and_(Predication.predicate_canonicalized == 'administered',
+                                                    and_(Predication.object_type == DOSAGE_FORM,
+                                                         Predication.subject_type.notin_(
+                                                             [SPECIES, DISEASE, CHEMICAL])))) \
+        .values(predicate_canonicalized=None)
+    session.execute(q_administered)
+    session.commit()
+
+    logging.info('Cleaning induces ([Chemical, Disease] -> [Chemical, Disease])')
     q_induces = update(Predication).where(and_(Predication.predicate_canonicalized == 'induces',
                                                or_(Predication.subject_type.notin_([CHEMICAL, DISEASE]),
                                                    Predication.object_type.notin_([CHEMICAL, DISEASE])))) \
@@ -125,7 +82,7 @@ def clean_extractions_in_database():
     session.execute(q_induces)
     session.commit()
 
-    logging.info('Cleaning decreases (Chemical -> ...)')
+    logging.info('Cleaning decreases ([Chemical, Disease] -> [Chemical, Disease])')
     q_decrease = update(Predication).where(and_(Predication.predicate_canonicalized == 'decreases',
                                                 or_(Predication.subject_type.notin_([CHEMICAL, DISEASE]),
                                                     Predication.object_type.notin_([CHEMICAL, DISEASE])))) \
@@ -133,7 +90,7 @@ def clean_extractions_in_database():
     session.execute(q_decrease)
     session.commit()
 
-    logging.info('Cleaning interacts (Chemical -> ...)')
+    logging.info('Cleaning interacts ([Chemical, Gene] -> [Chemical, Gene])')
     q_interacts = update(Predication).where(and_(Predication.predicate_canonicalized == 'interacts',
                                                  or_(Predication.subject_type.notin_([CHEMICAL, GENE]),
                                                      Predication.object_type.notin_([CHEMICAL, GENE])))) \
@@ -141,7 +98,7 @@ def clean_extractions_in_database():
     session.execute(q_interacts)
     session.commit()
 
-    logging.info('Cleaning metabolises (Chemical -> ...)')
+    logging.info('Cleaning metabolises (Gene -> Chemical)')
     q_metabolises = update(Predication).where(and_(Predication.predicate_canonicalized == 'metabolises',
                                                    or_(Predication.subject_type != GENE,
                                                        Predication.object_type != CHEMICAL))) \
@@ -158,13 +115,71 @@ def clean_extractions_in_database():
     session.commit()
 
     # Delete all treatments which are not (Chemical -> Disease, Species)
-    logging.info('Cleaning treats (Chemical -> Disease)...')
+    logging.info('Cleaning treats (Chemical -> [Disease, Species])...')
     q_treatment = update(Predication).where(and_(Predication.predicate_canonicalized == 'treats',
                                                  or_(Predication.subject_type != CHEMICAL,
-                                                     Predication.object_type.notin_([DISEASE, SPECIES]) != DISEASE))) \
+                                                     Predication.object_type.notin_([DISEASE, SPECIES])))) \
         .values(predicate_canonicalized=None)
     session.execute(q_treatment)
     session.commit()
+
+
+def mirror_symmetric_predicates():
+    """
+    Some predicates are symmetric - these predicates will be mirrored in the database
+    :return: None
+    """
+    session = Session.get()
+    logging.info('Mirroring symmetric predicates...')
+    for idx_pred, pred_to_mirror in enumerate(SYMMETRIC_PREDICATES):
+        start_time = datetime.now()
+        logging.info('Mirroring predicate: {}'.format(pred_to_mirror))
+        q = session.query(Predication)
+        q = q.filter(Predication.predicate_canonicalized == pred_to_mirror)
+        predication_values = []
+        count_mirrored = 0
+        for i, r in enumerate(session.execute(q)):
+            p = PredicationResult(*r)
+
+            try:
+                predication_values.append(dict(
+                    document_id=p.document_id,
+                    document_collection=p.document_collection,
+                    subject_id=p.object_id,
+                    subject_str=p.object_str,
+                    subject_type=p.object_type,
+                    predicate=p.predicate,
+                    predicate_canonicalized=p.predicate_canonicalized,
+                    object_id=p.subject_id,
+                    object_str=p.subject_str,
+                    object_type=p.subject_type,
+                    confidence=p.confidence,
+                    sentence_id=p.sentence_id,
+                    extraction_type=p.extraction_type
+                ))
+
+                if i % BULK_INSERT_AFTER_K == 0:
+                    session.bulk_insert_mappings(Predication, predication_values)
+                    session.commit()
+                    predication_values.clear()
+            except IntegrityError:
+                _insert_predication_skip_duplicates(session, predication_values)
+                predication_values.clear()
+
+            count_mirrored += 1
+        try:
+            # commit the remaining
+            session.bulk_insert_mappings(Predication, predication_values)
+            session.commit()
+            predication_values.clear()
+        except IntegrityError:
+            _insert_predication_skip_duplicates(session, predication_values)
+            predication_values.clear()
+
+        logging.info('{} facts mirrored for predicate: {} (in {}s)'.format(count_mirrored, pred_to_mirror,
+                                                                           datetime.now() - start_time))
+
+    logging.info('Mirroring predicates finished')
 
 
 def main():
@@ -173,8 +188,8 @@ def main():
                         level=logging.DEBUG)
 
     dosage_form_rule()
-    # mirror_symmetric_predicates()
     clean_extractions_in_database()
+    #mirror_symmetric_predicates()
 
 
 if __name__ == "__main__":
