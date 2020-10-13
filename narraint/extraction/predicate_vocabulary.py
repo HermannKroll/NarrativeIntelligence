161a478d7fe26850c828789b7c2908392a0264db
HermannKroll <hermann.kroll@gmail.com>
2020-10-13 15:38:08 +0200

    update vocab + new threshold

--- narraint/extraction/predicate_vocabulary.py
+++ narraint/extraction/predicate_vocabulary.py
@@ -3,15 +3,15 @@ def create_predicate_vocab():
                 associated=["contain", "produce", "convert", "yield", "isolate", "generate", "synthesize", "grow",
                             "occures", "evaluate", "augment", "effect", "develop", "affect", "contribute", "involve",
                             "associated with", "isa", "same as", "coexists with", "process", "method of", "part of",
-                            "associate", "correlate", "play role", "play"],
+                            "associate", "correlate", "play role", "play", "limit"],
                 compares=["compare with"],
-                induces=["stimulate", "increase", "potentiate", "enhance", "promote", "improve", "activate", "higher",
+                induces=["stimulate", "increase", "potentiate", "enhance", "activate", "higher",
                          "result", "lead", "causes"],
                 decreases=["reduce", "lower", "attenuate", "mediate"],
                 interacts=["bind", "interact", "target", "regulate"],
                 metabolises=["metabolite", "metabolize", "metabolise", "metabolism", "metabol"],
                 inhibits=["disrupt", "suppress", "inhibitor", "disturb"],
-                treats=["prevents", "use"],
+                treats=["prevents", "use", "improve", "promote", "sensitize", "sensitise", "aid"],
                 PRED_TO_REMOVE=["show", "present", "exhibit", "find", "form"])
 
 
