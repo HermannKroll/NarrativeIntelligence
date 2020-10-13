51fa468bcccd98bd6738c0e6a569e143e8bfb517
HermannKroll <hermann.kroll@gmail.com>
2020-10-13 15:52:38 +0200

    fix like search for DosageForms

--- narraint/queryengine/query_hints.py
+++ narraint/queryengine/query_hints.py
@@ -1,6 +1,8 @@
 from narraint.entity.enttypes import DISEASE, GENE, CHEMICAL, DOSAGE_FORM
 
-LIKE_SEARCH_FOR_ENTITY_TYPES = {DOSAGE_FORM, CHEMICAL, DISEASE, GENE}
+MESH_ONTOLOGY = 'MESH_ONTOLOGY'
+
+LIKE_SEARCH_FOR_ENTITY_TYPES = {DOSAGE_FORM, CHEMICAL, DISEASE, GENE, MESH_ONTOLOGY}
 
 DO_NOT_CARE_PREDICATE = 'associated'
 
@@ -10,3 +12,12 @@ PREDICATE_EXPANSION = dict(
 )
 
 SYMMETRIC_PREDICATES = {"interacts", "administered"}
+
+
+def should_perform_like_search_for_entity(entity_id, entity_type):
+    if entity_type == DOSAGE_FORM and entity_id.lower().startswith('fidx'):
+        return False
+    if entity_type in LIKE_SEARCH_FOR_ENTITY_TYPES:
+        return True
+    else:
+        return False
