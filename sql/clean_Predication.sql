47ac9efd917709d79af703e13d64c17f044051c5
HermannKroll <hermann.kroll@gmail.com>
2020-11-03 12:30:35 +0100

    table needs to be rewritten after to save disk space

--- sql/clean_Predication.sql
+++ sql/clean_Predication.sql
@@ -8,3 +8,6 @@ DELETE FROM PREDICATION WHERE (subject_str like '%_gely' and subject_type = 'Dos
 -- Chemicals with only a single character are mostly wrong tagged
 DELETE FROM PREDICATION WHERE (subject_str like '_' and subject_type = 'Chemical') or (object_str like '_' and object_type = 'Chemical');
 DELETE FROM PREDICATION WHERE (subject_str like '__' and subject_type = 'Chemical') or (object_str like '__' and object_type = 'Chemical');
+
+-- Rewrites the Predication table and deletes removed tuples
+VACUUM FULL PREDICATION;
