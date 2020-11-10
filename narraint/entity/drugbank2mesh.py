6e27bedc245c14b7a0e585cebc08dddf312037a8
HermannKroll <hermann.kroll@gmail.com>
2020-11-10 12:16:33 +0100

    drugbank mapping is now a 1:1 mapping (conflicts are resolved manually)

--- narraint/entity/drugbank2mesh.py
+++ narraint/entity/drugbank2mesh.py
@@ -6,6 +6,17 @@ from narraint.config import MESH_DESCRIPTORS_FILE, DRUGBANK_ID_2_MESH_MAPPING_IN
 from narraint.mesh.data import MeSHDB
 from narraint.preprocessing.tagging.drug import DrugTaggerVocabulary
 
+DRUGBANK_TO_MESH_DISAMBIGUATION = {
+    'DB03568': 'MESH:D020148',
+    'DB00693': 'MESH:D019793',
+    'DB00759': 'MESH:D013752',
+    'DB00091': 'MESH:D016572',
+    'DB04557': 'MESH:D016718',
+    'DB02134': 'MESH:D019820',
+    'DB03994': 'MESH:D019856',
+    'DB00536': 'MESH:D019791'
+}
+
 
 class DrugBank2MeSHMapper:
     __instance = None
@@ -20,23 +31,27 @@ class DrugBank2MeSHMapper:
     def __init__(self):
         self.drug_terms2dbid = {}
         self.mesh_terms2meshid = defaultdict(set)
-        self.dbid2meshid = defaultdict(set)
+        self.dbid2meshid = {}
 
     def compute_mappings(self):
         self._load_mesh_ontology()
         self.drug_terms2dbid = DrugTaggerVocabulary.create_drugbank_vocabulary_from_source(ignore_excipient_terms=1)
         # compute the intersection between both vocabs
         term_intersections = set(self.drug_terms2dbid.keys()).intersection(set(self.mesh_terms2meshid.keys()))
-        self.dbid2meshid = defaultdict(set)
+        self.dbid2meshid.clear()
         for term in term_intersections:
             dbids = self.drug_terms2dbid[term]
             mesh_ids = self.mesh_terms2meshid[term]
             for dbid in dbids:
                 if dbid in self.drug_terms2dbid:
-                    raise KeyError('DBID {}has alreay a mapping to {} (instead of {})'.format(dbid, mesh_ids,
-                                                                                              self.drug_terms2dbid[
-                                                                                                  dbid]))
-                self.dbid2meshid[dbid] = {f'MESH:{mesh_id}' for mesh_id in mesh_ids}
+                    raise KeyError('DBID {} has alreay a mapping to {} (instead of {})'.format(dbid, mesh_ids,
+                                                                                               self.drug_terms2dbid[
+                                                                                                   dbid]))
+                if len(mesh_ids) > 1:
+                    self.dbid2meshid[dbid] = DRUGBANK_TO_MESH_DISAMBIGUATION[dbid]
+                else:
+                    # this should map only a single element
+                    self.dbid2meshid[dbid] = mesh_ids.pop()
 
     def store_index(self, index_path=DRUGBANK_ID_2_MESH_MAPPING_INDEX):
         logging.info(f'Writing DrugBank2MeSH Mapping to cache: {index_path}')
@@ -56,8 +71,6 @@ class DrugBank2MeSHMapper:
         for desc in meshdb.get_all_descs():
             mesh_id, mesh_head = desc.unique_id, desc.heading
             mesh_mappings.append((mesh_id, mesh_head))
-            if mesh_id == 'D000068899':
-                print('stop')
             for term in desc.terms:
                 mesh_mappings.append((mesh_id, term.string))
 
