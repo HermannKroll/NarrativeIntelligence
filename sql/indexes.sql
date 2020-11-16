f8ded3c6c9a0d88d63a9e7300df32e9c42c8d0c6
HermannKroll <hermann.kroll@gmail.com>
2020-11-16 12:15:14 +0100

    change order of indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,9 +1,12 @@
+CREATE INDEX pred_predicate_idx ON PREDICATION(predicate);
+CREATE INDEX pred_predicate_canonicalized ON PREDICATION(predicate_canonicalized);
+
+
+
 CREATE INDEX pred_document_id_idx ON PREDICATION(document_id);
 CREATE INDEX pred_document_col_idx ON PREDICATION(document_collection);
 CREATE INDEX pred_subject_id_idx ON PREDICATION(subject_id);
 CREATE INDEX pred_subject_type_idx ON PREDICATION(subject_type);
-CREATE INDEX pred_predicate_idx ON PREDICATION(predicate);
-CREATE INDEX pred_predicate_canonicalized ON PREDICATION(predicate_canonicalized);
 CREATE INDEX pred_object_id_idx ON PREDICATION(object_id);
 CREATE INDEX pred_object_type_idx ON PREDICATION(object_type);
 CREATE INDEX pred_extraction_type_idx ON PREDICATION(extraction_type);
