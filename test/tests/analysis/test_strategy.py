1aff55aaa36ac78a2b6c5dfeb04e0a5dd1e4160f
jan <j.pirklbauer@tu-bs.de>
2020-11-18 10:06:21 +0100

    switched to unittest suites

--- test/tests/analysis/test_strategy.py
+++ test/tests/analysis/test_strategy.py
@@ -1,19 +1,21 @@
 from narraint.analysis.cikm2020.search.strategy import KeywordStrategy, KeywordDistanceStrategy
 from narraint.analysis.cikm2020.expert_evaluation import *
 import logging
+import unittest
 
 
-def test_keyword_perform_search():
-    strat = KeywordStrategy("../../resources/textstrategy_files")
-    prec, rec, fval = strat.perform_search(eval_q1, "PubMed", eval_q1_ids_met_dia, eval_q1_ids_correct)
-    logging.info(f"{prec}/{rec}/{fval}")
-    assert prec*rec > 0
+class TestSearchStrategies(unittest.TestCase):
+    def test_keyword_perform_search(self):
+        strat = KeywordStrategy("../../resources/textstrategy_files")
+        prec, rec, fval = strat.perform_search(eval_q1, "PubMed", eval_q1_ids_met_dia, eval_q1_ids_correct)
+        logging.info(f"{prec}/{rec}/{fval}")
+        assert prec*rec > 0
 
-def test_keyword_distance_perform_search():
-    results = dict()
-    for distance in range(0, 5000, 50):
-        strat = KeywordDistanceStrategy("../../resources/textstrategy_files",distance)
-        results[distance] = strat.perform_search(eval_q1, "PubMed", eval_q1_ids_met_dia, eval_q1_ids_correct)
-    logging.info(results)
+    def test_keyword_distance_perform_search(self):
+        results = dict()
+        for distance in range(0, 5000, 50):
+            strat = KeywordDistanceStrategy("../../resources/textstrategy_files",distance)
+            results[distance] = strat.perform_search(eval_q1, "PubMed", eval_q1_ids_met_dia, eval_q1_ids_correct)
+        logging.info(results)
 
 
--- test/tests/backend/test_covidexport.py
+++ test/tests/backend/test_covidexport.py
@@ -1,10 +1,12 @@
+import unittest
 from narraint.backend.covidexport import create_sanitized_index
 
-def test_create_sanitized_index():
-    text = "Das ist ein schön€r Tag!"
-    tag_start = 8
-    tag_end = 18
-    index = create_sanitized_index(text)
-    san_start = index[tag_start]
-    san_end = index[tag_end]
-    assert text[san_start:san_end] == "ein schön€r"
+class TestCovidExport(unittest.TestCase):
+    def test_create_sanitized_index(self):
+        text = "Das ist ein schön€r Tag!"
+        tag_start = 8
+        tag_end = 18
+        index = create_sanitized_index(text)
+        san_start = index[tag_start]
+        san_end = index[tag_end]
+        assert text[san_start:san_end] == "ein schön€r"
