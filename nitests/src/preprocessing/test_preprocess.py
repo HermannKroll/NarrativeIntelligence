0360b81d8f8d95a8efa4bd1b885799a55b50a5c2
jan <j.pirklbauer@tu-bs.de>
2020-11-19 15:21:59 +0100

    preprocess running in tests - but no tags so far

--- nitests/src/preprocessing/test_preprocess.py
+++ nitests/src/preprocessing/test_preprocess.py
@@ -1,8 +1,21 @@
+import os
 import unittest
+import pytest
 
+import nitests.config.config as config
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+from narraint.preprocessing import preprocess
+from narraint.tools import proj_rel_path
+
+class TestPreprocess(unittest.TestCase):
+    def test_single_file_DR(self):
+        self.outputdir = config.make_test_tempdir()
+        self.workdir = config.make_test_tempdir()
+        args = [proj_rel_path('nitests/resources/infiles/PMC1313813Untagged.txt'),
+                os.path.join(self.outputdir, "output.txt"),
+                *f"-t DR C D DF -c PREPTEST --loglevel DEBUG --workdir {self.workdir}".split()
+                ]
+        preprocess.main(args)
         self.assertEqual(True, False)
 
 
