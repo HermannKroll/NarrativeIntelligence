2315e253376e9cf17d35f9acc7e9188ea94a6d8c
HermannKroll <hermann.kroll@gmail.com>
2021-01-11 13:47:37 +0100

    remove glycine from test cases

--- nitests/src/preprocessing/tagging/test_metadictagger.py
+++ nitests/src/preprocessing/tagging/test_metadictagger.py
@@ -16,7 +16,6 @@ class TestMetadictagger(unittest.TestCase):
     def test_init(self):
         metatag = TestMetadictagger.make_metatag()
         self.assertSetEqual(set(metatag._vocabs.keys()), TestMetadictagger.ent_type_set)
-        self.assertIn("DB00145", metatag._vocabs[et.EXCIPIENT]["glycine"])
         self.assertIn("MESH:D007267", metatag._vocabs[et.DOSAGE_FORM]["injection"])
         self.assertIn("Agave", metatag._vocabs[et.PLANT_FAMILY]["agave"])
         self.assertIn("DB06727", metatag._vocabs[et.DRUG]["sparteine"])
@@ -50,9 +49,7 @@ class TestMetadictagger(unittest.TestCase):
 def assert_tags_pmc_4297_5600(test_suit, tags_4297, tags_5600):
     test_suit.assertIn("<Entity 399,426,intraventricular injection,DosageForm,MESH:D007276>", tags_4297)
     test_suit.assertIn("<Entity 636,646,injection,DosageForm,MESH:D007267>", tags_4297)
-    test_suit.assertIn("<Entity 743,751,glycine,Excipient,DB00145>", tags_4297)
-    test_suit.assertIn("<Entity 657,665,glycine,PlantFamily,Glycine>", tags_4297)
-
+   
     test_suit.assertIn("<Entity 209,219,sparteine,Drug,DB06727>", tags_5600)
     test_suit.assertIn("<Entity 189,205,4-aminopyridine,Drug,DB06637>", tags_5600)
     test_suit.assertIn("<Entity 826,836,potassium,Excipient,DB14500>", tags_5600)
