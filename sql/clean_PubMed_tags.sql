442365a4a3ac65338e4e182b8cfe472d124a5a71
HermannKroll <hermann.kroll@gmail.com>
2021-03-18 11:40:22 +0100

    more cleaning rules

--- sql/clean_PubMed_tags.sql
+++ sql/clean_PubMed_tags.sql
@@ -1,6 +1,3 @@
-DELETE FROM TAG where ent_id = '-';
-
-
 WITH doc AS (
     SELECT id, (length(title) + length(abstract) + 10) as length
     from document d
@@ -12,3 +9,12 @@ FROM tag t
 WHERE t.document_id = doc.id
   AND t.end > doc.length
   AND t.document_collection = 'PubMed';
+
+
+DELETE FROM TAG where ent_id = '-';
+
+DELETE FROM tag WHERE ent_type = 'Chemical';
+DELETE FROM doc_tagged_by WHERE ent_type = 'Chemical';
+
+DELETE FROM tag WHERE ent_id like 'OMIM:%';
+DELETE FROM doc_tagged_by WHERE ent_type = 'Disease';
