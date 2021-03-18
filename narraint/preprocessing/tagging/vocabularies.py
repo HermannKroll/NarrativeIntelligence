3b837e368efc75ce5872e1725079a638aa3a48b2
HermannKroll <hermann.kroll@gmail.com>
2021-03-18 11:18:06 +0100

    check if assay was included

--- narraint/preprocessing/tagging/vocabularies.py
+++ narraint/preprocessing/tagging/vocabularies.py
@@ -108,7 +108,10 @@ class LabMethodVocabulary:
     @staticmethod
     def create_lab_method_vocabulary(mesh_file=MESH_DESCRIPTORS_FILE, expand_terms=True):
         term2desc = MethodVocabulary.create_method_vocabulary(mesh_file, expand_terms=expand_terms, method_type=LAB_METHOD)
-        term2desc['assay'].append('FIDXPM1')
+        if 'assay' not in term2desc:
+            term2desc['assay'] = ['FIDXLM1']
+        else:
+            term2desc['assay'].append('FIDXLM1')
         return term2desc
 
 
