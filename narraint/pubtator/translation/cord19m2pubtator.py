28b086af728bab477d65ab25c6e216782573afa2
HermannKroll <hermann.kroll@gmail.com>
2021-04-08 08:51:31 +0200

    update new conversion

--- narraint/pubtator/translation/cord19m2pubtator.py
+++ narraint/pubtator/translation/cord19m2pubtator.py
@@ -1,30 +1,31 @@
 import logging
-import os
-import tempfile
 from argparse import ArgumentParser
 
-from narraint.config import PREPROCESS_CONFIG
-from narraint.preprocessing.config import Config
-from narraint.preprocessing.convertids import load_pmcids_to_pmid_index
-from narraint.pubtator.translation.csv2pubtator import CsvConverter
+import csv
+from itertools import islice
+
+from narraint.backend.models import Document
 
 
 def main():
-    parser = ArgumentParser(description="Collect and convert PMC files from a list of pmc-ids")
-    parser.add_argument("input", help="File containing PMC IDs OR directory containing PMC files")
-    parser.add_argument("output", help="Directory to output the converted Files into")
+    parser = ArgumentParser(description="converts the cord19 metadata csv file...")
+    parser.add_argument("input", help="CORD19 metadata csv file")
+    parser.add_argument("output", help="output as a pubtator file")
     group_settings = parser.add_argument_group("Settings")
-    group_settings.add_argument("--config", default=PREPROCESS_CONFIG,
-                                help="Configuration file (default: {})".format(PREPROCESS_CONFIG))
     group_settings.add_argument("--loglevel", default="INFO")
 
     args = parser.parse_args()
-    conf = Config(args.config)
     logging.basicConfig(level=args.loglevel.upper())
-    logging.info('Load PMCID to PMID translation file')
-    pmcid2pmid = load_pmcids_to_pmid_index(conf.pmcid2pmid)
-    translator = CsvConverter(primary_index=0, id_index=6, title_index=3, abstract_index=8, pmc_index=5, pmcids2pmids=pmcid2pmid)
-    translator.convert(args.input, args.output)
+
+    with open(args.input, 'rt') as input_file:
+        with open(args.output, 'wt') as output_file:
+            reader = csv.reader(input_file)
+            doc_id = 1
+            for row in islice(reader, 1, None):
+                title, abstract = row[3].strip(), row[8].strip()
+                output_file.write(Document.create_pubtator(doc_id, title, abstract))
+                output_file.write('\n')
+                doc_id = doc_id + 1
 
 
 if __name__ == "__main__":
