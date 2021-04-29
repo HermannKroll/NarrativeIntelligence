a287f39a6a1c9687b784f92759d4b7ab0aa9cf94
jan <j.pirklbauer@tu-bs.de>
2021-04-29 13:13:24 +0200

    Removed deprecated metadictagger preprocess tests

--- nitests/src/preprocessing/test_preprocess.py
+++ nitests/src/preprocessing/test_preprocess.py
@@ -16,33 +16,6 @@ from nitests.src.preprocessing.tagging.test_metadictagger import assert_tags_pmc
 
 class TestPreprocess(unittest.TestCase):
 
-    def test_metadictagger(self):
-        self.output = os.path.join(nitests.util.make_test_tempdir(), "output.txt")
-        logging.info(self.output)
-        self.workdir = nitests.util.make_test_tempdir()
-        args = [proj_rel_path('nitests/resources/infiles/test_metadictagger'),
-                #self.output,
-                *f"-t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {self.workdir}".split()
-                ]
-        preprocess.main(args)
-        doc1, doc2 = util.get_tags_from_database(4297), util.get_tags_from_database(5600)
-        assert_tags_pmc_4297_5600(self, {repr(t) for t in doc1}, {repr(t) for t in doc2})
-        util.clear_database()
-
-
-    #pytest.mark.skip(reason="export broke")
-    def test_metadictagger_parallel(self):
-        self.output = os.path.join(nitests.util.make_test_tempdir(), "output.txt")
-        self.workdir = nitests.util.make_test_tempdir()
-        args = [proj_rel_path('nitests/resources/infiles/test_metadictagger'),
-                #self.output,
-                *f"-t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {self.workdir} -w 2".split()
-                ]
-        preprocess.main(args)
-        doc1, doc2 = util.get_tags_from_database(4297), util.get_tags_from_database(5600)
-        assert_tags_pmc_4297_5600(self, {repr(t) for t in doc1}, {repr(t) for t in doc2})
-        util.clear_database()
-
     def test_dictpreprocess_sinlge_worker(self):
         workdir = nitests.util.make_test_tempdir()
         args = [proj_rel_path('nitests/resources/infiles/test_metadictagger'),
