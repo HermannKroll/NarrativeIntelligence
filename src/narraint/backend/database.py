44f32eec35b2301a8819f11009d0bbe3b50d7dda
HermannKroll <hermann.kroll@gmail.com>
2021-06-24 10:22:45 +0200

    support fancy bulk insert for all tables

--- src/narraint/backend/database.py
+++ src/narraint/backend/database.py
@@ -4,13 +4,14 @@ from narrant.backend.database import Session
 
 
 class SessionExtended(Session):
+    is_sqlite = False
+    is_postgres = False
 
     @classmethod
     def get(cls, connection_config: str = BACKEND_CONFIG, declarative_base=Extended):
         if not cls._instance:
             cls._instance = Session(connection_config, declarative_base)
+            SessionExtended.is_postgres = cls._instance.is_postgres
+            SessionExtended.is_sqlite = cls._instance.is_sqlite
         return cls._instance.session
 
-    @classmethod
-    def is_postgres(cls):
-        return cls._instance.is_postgres()
