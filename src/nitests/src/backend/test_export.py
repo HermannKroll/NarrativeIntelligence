98edfa5566c8ba693d4c22fcdb5c4ca1263886ed
jan <j.pirklbauer@tu-bs.de>
2021-07-02 08:19:19 +0200

    export now supports new json export format. added export tests for pubtator and json format

---
+++ src/nitests/src/backend/test_export.py
@@ -0,0 +1,34 @@
+import unittest
+import pytest
+
+from narraint.backend.database import SessionExtended
+from narrant.backend.load_document import document_bulk_load
+from narrant.pubtator.document import TaggedDocument
+from narrant.backend.export import export
+from nitests import util
+
+
+def setup_module(module):
+    test_mapping= {"Drug":("Drugtagger", "1.0"), "Disease":("Diseasetagger", "1.0")}
+    document_bulk_load(util.get_test_resource_filepath("infiles/test_export/in/"), "TEST", test_mapping)
+    session = SessionExtended.get()
+    pass
+
+def teardown_module(module):
+    util.clear_database()
+
+class TestExport(unittest.TestCase):
+    def test_export_pubtator(self):
+        outfile = util.tmp_rel_path("export_out")
+        testfile = util.get_test_resource_filepath("infiles/test_export/out/pubtator.txt")
+        export(outfile, ["Drug", "Disease"], export_format="pubtator")
+        with open(outfile) as of, open(testfile) as tf:
+            self.assertEqual(tf.read(), of.read())
+
+    def test_export_json(self):
+        outfile = util.tmp_rel_path("export_out")
+        testfile = util.get_test_resource_filepath("infiles/test_export/out/json.txt")
+
+        export(outfile, ["Drug", "Disease"], export_format="json")
+        with open(outfile) as of, open(testfile) as tf:
+            self.assertEqual(tf.read(), of.read())
