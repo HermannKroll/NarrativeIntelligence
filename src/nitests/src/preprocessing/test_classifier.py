f44f83d5850b2dd178e11c07cadb718eaf7c9421
jan <j.pirklbauer@tu-bs.de>
2021-07-19 15:20:08 +0200

    implemented classification pipeline

---
+++ src/nitests/src/preprocessing/test_classifier.py
@@ -0,0 +1,37 @@
+import unittest
+
+import re
+
+from narrant.preprocessing.classifier import Classifyer
+from narrant.pubtator.document import TaggedDocument
+from nitests import util
+
+
+class TestClassfier(unittest.TestCase):
+    pet_rules = util.get_test_resource_filepath("classifier_rules/testrules.txt")
+
+    def test_read_ruleset(self):
+        rules = Classifyer.read_ruleset(TestClassfier.pet_rules)
+        self.assertIn([re.compile(r"kitten\w+")], rules)
+        self.assertIn([re.compile(r"dog\w+")], rules)
+        self.assertIn([re.compile(r"hamster")], rules)
+        self.assertIn([re.compile(r"animal"), re.compile("house")], rules)
+
+    def test_classify(self):
+        classfier = Classifyer("pet", rule_path=TestClassfier.pet_rules)
+        positive_docs = [
+            TaggedDocument(title="some animals", abstract="Some people keep an animal in their house."),
+            TaggedDocument(title="a cute hamster", abstract=""),
+            TaggedDocument(title="two kittens for sale")
+        ]
+        negative_docs = [
+            TaggedDocument(title="this has nothing to do with an animal"),
+            TaggedDocument(title="this is about hamsters")
+        ]
+        for doc in positive_docs + negative_docs:
+            classfier.classify_document(doc)
+
+        for doc in positive_docs:
+            self.assertIn('pet', doc.classification)
+        for doc in negative_docs:
+            self.assertNotIn('pet', doc.classification, msg=f"{doc}: false positive")
