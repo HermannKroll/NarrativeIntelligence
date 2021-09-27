eb923530f9ced7e36e9fc62143ed5fb443fb93ef
HermannKroll <hermann.kroll@gmail.com>
2021-09-27 11:51:54 +0200

    refactor to ignore result argument

--- src/narraint/queryengine/aggregation/ontology.py
+++ src/narraint/queryengine/aggregation/ontology.py
@@ -105,8 +105,8 @@ class ResultAggregationByOntology(QueryResultAggregationStrategy):
                 if misc_document_results:
                     for ent_type, document_results in misc_document_results.items():
                         document_results = misc_document_results[ent_type]
-                        misc_aggregation_list = self.substitution_based_strategy.rank_results(document_results,
-                                                                                              freq_sort_desc)[0]
+                        misc_aggregation_list, _ = self.substitution_based_strategy.rank_results(document_results,
+                                                                                              freq_sort_desc)
                         misc_aggregation = self._create_query_aggregate("", "", f'{ent_type} (No MeSH Taxonomy)',
                                                                         f'{ent_type} (No MeSH Taxonomy)')
                         misc_aggregation.add_query_result(misc_aggregation_list)
