04b8851cfa5e7421a11243824420b510da52bded
jan <j.pirklbauer@tu-bs.de>
2021-11-03 14:07:43 +0100

    Bugfixing and added load option

--- src/narraint/pollux/translate_pollux.py
+++ src/narraint/pollux/translate_pollux.py
@@ -1,6 +1,7 @@
 import datetime
 import hashlib
 import json
+import logging
 from collections import namedtuple
 from dataclasses import dataclass
 from operator import and_
@@ -10,9 +11,11 @@ import argparse
 from pathlib import Path
 
 import narraint.backend.database as db
+import narrant.backend.load_document as load_document
 from sqlalchemy import func, select
 from kgextractiontoolbox.document.document import TaggedDocument
 from narraint.backend.models import DocumentTranslation
+from narrant.progress import Progress
 
 
 def main(args=None):
@@ -20,10 +23,22 @@ def main(args=None):
     parser.add_argument("input", help="ijson input file")
     parser.add_argument("output", help="output json file")
     parser.add_argument("-c", "--collection", required=True, help="document collection")
+    parser.add_argument("-d", "--diff", action="store_true", help="only process documents with new/changed md5 hash")
+    parser.add_argument("-l", "--load", action="store_true", help="load document contents into document table")
     args = parser.parse_args()
 
     loader = PolluxLoader(args.collection)
-    loader.translate(args.input, args.output)
+    logging.info("Pollux document translation")
+    logging.debug(f"Input file: {args.input}")
+    logging.debug(f"Output file: {args.output}")
+    logging.info("Counting documents...")
+    count = loader.count_documents(args.input)
+    logging.info(f"Found {count} documents.")
+    prog = Progress(total=count, text="Translating")
+    proc_docs = loader.translate(args.input, args.output, diff=args.diff, prog_logger=prog)
+    logging.info(f"Processed {proc_docs} new or changed documents.")
+    if args.load:
+        load_document.main([args.output, "-c", args.collection])
     #loader.read_tagged_documents(args.input)
 
 @dataclass
@@ -59,7 +74,10 @@ class ConversionLoader:
             ))
         )
         result = [r for r in result]
-        return not result or result != self.get_md5(doc)
+        if not result:
+            return True
+        else:
+            return result[0][0] != self.get_md5(doc)
 
     def create_translation_entry(self, sdoc:SourcedDocument):
         sdoc.doc.id = self.current_art_id
@@ -73,18 +91,30 @@ class ConversionLoader:
             "date_inserted": self.insertion_time
         }
 
-    def translate(self, infile: Union[Path, str], outfile: Union[Path, str], insert_every=100):
+    def translate(self, infile: Union[Path, str], outfile: Union[Path, str], insert_every=100, diff=False, prog_logger: Progress=None):
         translations = []
+        processed_docs = 0
         with open(outfile, "w+") as outf:
+            prog_logger.start_time()
             outf.write("[")
-            for sdoc in self.read_sourced_documents(infile):
-                if self.check_md5_changed(sdoc):
+            first = True
+            for n, sdoc in enumerate(self.read_sourced_documents(infile)):
+                if not diff or self.check_md5_changed(sdoc):
                     translations.append(self.create_translation_entry(sdoc))
-                    outf.write(json.dumps(sdoc.doc.to_dict()) + ",\n")
+                    if not first:
+                        outf.write(",\n")
+                    else:
+                        first=False
+                    outf.write(json.dumps(sdoc.doc.to_dict()))
+                    processed_docs +=1
                     if len(translations) > 100:
                         self.flush(translations)
                         translations = []
+                prog_logger.print_progress(n)
             self.flush(translations)
+            outf.write("]")
+            prog_logger.done()
+            return processed_docs
 
     def flush(self, translations):
         self.session.bulk_insert_mappings(DocumentTranslation, translations)
@@ -95,6 +125,9 @@ class ConversionLoader:
         return hashlib.md5((sdoc.doc.title+sdoc.doc.abstract).encode('unicode_escape')).hexdigest()
 
     def read_sourced_documents(self, file: Union[Path, str]):
+        raise NotImplementedError()
+
+    def count_documents(self, file: Union[Path, str]):
         raise NotImplementedError()
 
 class PolluxLoader(ConversionLoader):
@@ -116,9 +149,15 @@ class PolluxLoader(ConversionLoader):
                         'unicode_escape'))
                     yield SourcedDocument(f"{source_id}:{n}", source, doc)
 
-
+    def count_documents(self, file: Union[Path, str]):
+        count = 0
+        for line in open(file): count += 1
+        return count
 
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level="INFO")
     main()
+    # 13cd3a58fa8cd5360f252c766d305248
+    # 13cd3a58fa8cd5360f252c766d305248
