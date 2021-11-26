3ee8af5c5e7757564efce825f259e3b547c313c3
jan <j.pirklbauer@tu-bs.de>
2021-11-26 07:43:57 +0100

    only use documents with only english text

--- src/narraint/pollux/translate_pollux.py
+++ src/narraint/pollux/translate_pollux.py
@@ -18,7 +18,7 @@ class PolluxLoader(DocumentTranslationLoader):
         with open(file) as f:
             for line in f:
                 content = json.loads(line)
-                if not content["languages"] or 'eng' not in content["languages"]:
+                if not content["languages"] or 'eng' not in content["languages"] or len(content["languages"]) > 1:
                     continue
                 source_id = content["id"]
                 source = file.name
