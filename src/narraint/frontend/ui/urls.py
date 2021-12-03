e9e7113405f609c16bb264d19b826a8217bc23e0
HermannKroll <hermann.kroll@gmail.com>
2021-12-03 10:06:35 +0100

    add the route

--- src/narraint/frontend/ui/urls.py
+++ src/narraint/frontend/ui/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.views.decorators.cache import never_cache
 
 from narraint.frontend.ui.views import SearchView, get_autocompletion, get_check_query, get_query, get_feedback, \
-    post_report, get_provenance, get_document_graph, get_tree_info
+    post_report, get_provenance, get_document_graph, get_tree_info, get_query_sub_count
 
 urlpatterns = [
     path("", never_cache(SearchView.as_view()), name="search"),
     path("query", get_query, name="query"),
+    path("query_sub_count", get_query_sub_count, name="query_sub_count"),
     path("autocompletion", get_autocompletion, name="autocompletion"),
     path("check", get_check_query, name="check"),
     path("feedback", get_feedback, name="feedback"),
