40edc0ba2a5862ee411bdf24ca5f9cf60925bc33
HermannKroll <hermann.kroll@gmail.com>
2021-12-18 10:45:23 +0100

    fix test database tests

--- src/nitests/src/backend/test_database.py
+++ src/nitests/src/backend/test_database.py
@@ -1,7 +1,9 @@
+import json
 import os
 import unittest
 
 from narraint.backend.database import SessionExtended
+from narraint.config import BACKEND_CONFIG
 from narrant.backend.models import Tagger
 from nitests.util import tmp_rel_path
 
@@ -10,11 +12,21 @@ class TestSession(unittest.TestCase):
 
     def test_sqlite_ins_sel(self):
         session = SessionExtended.get()
+        session.execute("DELETE FROM tagger")
+        session.commit()
         session.execute("INSERT INTO tagger VALUES ('foo', 'bar')")
         result = session.query(Tagger)
         for row in result:
             self.assertTrue(row.name == 'foo' and row.version == 'bar')
-        self.assertTrue(os.path.isfile(tmp_rel_path("sqlite.db")))
+
+    def test_is_sqllite(self):
+        session = SessionExtended.get()
+        self.assertTrue(SessionExtended.is_sqlite)
+
+    def test_correct_file_exists(self):
+        with open(BACKEND_CONFIG) as f:
+            config = json.load(f)
+        self.assertTrue(os.path.isfile(config["SQLite_path"]))
 
 
 if __name__ == '__main__':
