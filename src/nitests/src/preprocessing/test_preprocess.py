ce7773412bfe38abe4517c5f6625675bd84aac13
HermannKroll <hermann.kroll@gmail.com>
2021-12-19 14:04:20 +0100

    update dict

--- src/nitests/src/preprocessing/test_preprocess.py
+++ src/nitests/src/preprocessing/test_preprocess.py
@@ -3,6 +3,7 @@ import unittest
 import pytest
 
 import nitests.util
+from narrant.backend.load_document import document_bulk_load
 from narrant.preprocessing import preprocess, dictpreprocess
 from nitests import util
 from nitests.src.preprocessing.tagging.test_metadictagger import assert_tags_pmc_4297_5600
@@ -12,21 +13,29 @@ class TestPreprocess(unittest.TestCase):
 
     def test_dictpreprocess_json_input(self):
         workdir = nitests.util.make_test_tempdir()
-        args = [util.resource_rel_path('infiles/json_infiles'),
-
-                *f"-t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {workdir} -w 1 -y".split()
-                ]
+        args = [
+            *f"-i {util.resource_rel_path('infiles/json_infiles')} -t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {workdir} -w 1 -y".split()
+        ]
         dictpreprocess.main(args)
         doc1, doc2 = util.get_tags_from_database(5297), util.get_tags_from_database(5600)
         assert_tags_pmc_4297_5600(self, {repr(t) for t in doc1}, {repr(t) for t in doc2})
         util.clear_database()
 
-    def test_dictpreprocess_sinlge_worker(self):
+    def test_dictpreprocess_sinlge_worker_from_file(self):
         workdir = nitests.util.make_test_tempdir()
-        args = [util.resource_rel_path('infiles/test_metadictagger'),
+        path = util.resource_rel_path('infiles/test_metadictagger')
+        args = [
+            *f"-i {path} -t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {workdir} -w 1 -y".split()
+            ]
+        dictpreprocess.main(args)
+        doc1, doc2 = util.get_tags_from_database(4297), util.get_tags_from_database(5600)
+        assert_tags_pmc_4297_5600(self, {repr(t) for t in doc1}, {repr(t) for t in doc2})
+        util.clear_database()
 
-                *f"-t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {workdir} -w 1 -y".split()
-                ]
+    def test_dictpreprocess_sinlge_worker_from_database(self):
+        workdir = nitests.util.make_test_tempdir()
+        document_bulk_load(util.resource_rel_path('infiles/test_metadictagger'), collection="DBINSERTTAGGINGTEST")
+        args = [*f"-t DR DF PF E -c DBINSERTTAGGINGTEST --loglevel DEBUG --workdir {workdir} -w 1 -y".split()]
         dictpreprocess.main(args)
         doc1, doc2 = util.get_tags_from_database(4297), util.get_tags_from_database(5600)
         assert_tags_pmc_4297_5600(self, {repr(t) for t in doc1}, {repr(t) for t in doc2})
@@ -34,10 +43,9 @@ class TestPreprocess(unittest.TestCase):
 
     def test_dictpreprocess_dual_worker(self):
         workdir = nitests.util.make_test_tempdir()
-        args = [util.resource_rel_path('infiles/test_metadictagger'),
-
-                *f"-t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {workdir} -w 2 -y".split()
-                ]
+        args = [
+            *f"-i {util.resource_rel_path('infiles/test_metadictagger')} -t DR DF PF E -c PREPTEST --loglevel DEBUG --workdir {workdir} -w 2 -y".split()
+        ]
         dictpreprocess.main(args)
         doc1, doc2 = util.get_tags_from_database(4297), util.get_tags_from_database(5600)
         assert_tags_pmc_4297_5600(self, {repr(t) for t in doc1}, {repr(t) for t in doc2})
