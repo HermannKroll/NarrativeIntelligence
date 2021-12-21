b951faab672754e87bb391508b6ad583e7d2c754
HermannKroll <hermann.kroll@gmail.com>
2021-12-21 15:19:35 +0100

    Update wiki_queries.sql

--- src/narraint/pollux/wiki_queries.sql
+++ src/narraint/pollux/wiki_queries.sql
@@ -43,6 +43,9 @@ DELETE FROM Tag
 WHERE document_collection = 'scientists'
 and ent_str IN ('the', 'one', 'two', 'and', 'he', 'three', 'it', 'a', 'four', 'to', 'de',  'e', 'b', 'd');
 
+DELETE FROM Tag
+WHERE document_collection = 'scientists' and length(ent_str) < 5;
+
 
 DELETE FROM Predication
 WHERE document_collection = 'scientists'
@@ -73,13 +76,19 @@ WHERE
 relation = 'P166' -- is not null
 and Predication.document_collection = 'scientists'
 and subject_type = 'Person' and object_type = 'Award' and object_str not in ('award','awards', 'diploma', 'winner')
-ORDER by subject_type, relation, object_type
+ORDER by subject_type, relation, object_type;
 
-
+-- Without Entity Filter
 SELECT *
 FROM Predication
 WHERE extraction_type = 'OpenIE' and document_collection = 'scientists'
-ORDER by subject_type, relation, object_type
+ORDER by subject_type, relation, object_type;
+
+-- With entity filter
+SELECT *
+FROM Predication
+WHERE extraction_type = 'OpenIE51' and document_collection = 'scientists'
+ORDER by subject_type, relation, object_type;
 
 
 SELECT *
@@ -88,3 +97,67 @@ WHERe relation is not null;
 
 DELETE FROM doc_processed_by_ie WHERE document_collection = 'scientists';
 DELETE FROM predication WHERE document_collection = 'scientists';
+
+
+SELECT subject_str, predicate, relation, object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE51_NoFilter' and Predication.document_collection = 'scientists'
+ORDER by random()
+LIMIT 100;
+
+
+SELECT subject_str, predicate, relation, object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE51' and subject_type = 'Person' and Predication.document_collection = 'scientists'
+ORDER by random()
+LIMIT 100;
+
+
+SELECT subject_str, predicate, relation, object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE51_ExactFilter'  and Predication.document_collection = 'scientists'
+ORDER by random()
+LIMIT 100;
+
+
+
+
+SELECT subject_id, subject_str, predicate, object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE_SubjectExact'  and Predication.document_collection = 'scientists'
+and subject_id like '%wikidata%' and subject_type = 'Person'
+ORDER by random()
+LIMIT 100;
+
+
+-- max sentence length
+SELECT subject_id, subject_str, predicate, object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE_SubjectExact'  and Predication.document_collection = 'scientists'
+and subject_id like '%wikidata%' and subject_type = 'Person' and length(s.text) < 100
+ORDER by random()
+LIMIT 100;
+
+-- max sentence length
+SELECT subject_id, subject_str, predicate,	 object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE6_SubjectExact'  and Predication.document_collection = 'scientists'
+and subject_id like '%wikidata%' and subject_type = 'Person' and length(s.text) < 150 and subject_id = 'http://www.wikidata.org/entity/Q937'
+ORDER by random()
+LIMIT 100;
+
+
+SELECT extraction_type, predicate, relation, count(*)
+FROM Predication
+WHERE Predication.document_collection = 'scientists'  AND relation is not null
+GROUP by extraction_type, predicate, relation
+ORDER BY COUNT(*) DESC;
+
+
+SELECT subject_id, subject_str, predicate, relation, object_str, s.text
+FROM Predication join Sentence s on (Predication.sentence_id = s.id)
+WHERE extraction_type = 'OpenIE6_SubjectExact'  and Predication.document_collection = 'scientists'
+and subject_id like '%wikidata%' and subject_type = 'Person' and length(s.text) < 150 and subject_id = 'http://www.wikidata.org/entity/Q937'
+and relation is not null
+ORDER by random()
+LIMIT 100;
