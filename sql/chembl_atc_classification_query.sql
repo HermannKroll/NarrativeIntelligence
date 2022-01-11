3542a3b063bde2a6a1622e5f7f8470a03a2ddb4e
HermannKroll <hermann.kroll@gmail.com>
2022-01-11 09:50:53 +0100

    add query to generate atc classification csv

--- sql/chembl_atc_classification_query.sql
+++ sql/chembl_atc_classification_query.sql
@@ -1,4 +1,6 @@
-select chembl_id, ac.level4, ac.level4_description
+select chembl_id, who_name, ac.level1, ac.level2, ac.level3, ac.level4, ac.level5,
+ac.level1_description, ac.level2_description, ac.level3_description, ac.level4_description
 from molecule_dictionary md
 join molecule_atc_classification mac on md.molregno = mac.molregno
 join atc_classification ac on mac.level5 = ac.level5
+ORDER BY chembl_id ASC
