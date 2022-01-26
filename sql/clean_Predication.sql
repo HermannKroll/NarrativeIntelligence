6c6782e605a280f3f7da86742699beaa1ddf5596
HermannKroll <hermann.kroll@gmail.com>
2022-01-26 12:06:20 +0100

    move some cleaning rules to sql file (easier to maintain)

--- sql/clean_Predication.sql
+++ sql/clean_Predication.sql
@@ -1,3 +1,17 @@
+-- Update Dosage Form
+UPDATE public.Predication SET relation = 'administered' WHERE subject_type = 'DosageForm' or object_type = 'DosageForm';
+
+-- Update Method and LabMethod relation
+UPDATE public.Predication SET relation = 'method' WHERE subject_type = 'Method' or object_type = 'Method';
+UPDATE public.Predication SET relation = 'method' WHERE subject_type = 'LabMethod' or object_type = 'LabMethod';
+
+-- Delete all symmetric predications (subject = object)
+DELETE FROM public.Predication AS p WHERE p.subject_id = p.object_id and p.subject_type = p.object_type;
+
+-- Update all non-relations
+UPDATE public.Predication SET relation = 'associated' WHERE relation IS null;
+
+
 UPDATE Predication SET predicate_canonicalized = 'associated_unsure' WHERE predicate_canonicalized = 'PRED_TO_REMOVE';
 UPDATE PREDICATION SET subject_id = lower(subject_id) WHERE subject_type = 'Gene';
 UPDATE PREDICATION SET object_id = lower(object_id) WHERE object_type = 'Gene';
