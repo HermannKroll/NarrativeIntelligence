2017693b7a4c9ccbd6bb9ea1cbd159652ab3cc3e
jan <j.pirklbauer@tu-bs.de>
2022-03-29 10:12:34 +0200

    removed duplicate backend, cleaning and extraction tests from nitests

--- src/nitests/src/cleaning/test_relation_type_constraint_checking.py
+++ src/nitests/src/cleaning/test_relation_type_constraint_checking.py
@@ -42,27 +42,3 @@ class RelationTypeConstraintChecking(unittest.TestCase):
             self.assertEqual(objects, objects.intersection(objects_store), msg=f'comparison failed for: {relation}')
             self.assertEqual(objects_store, objects.intersection(objects_store),
                              msg=f'comparison failed for: {relation}')
-
-    def test_missing_object_constraints_1(self):
-        store = RelationTypeConstraintStore()
-        with self.assertRaises(ValueError):
-            store.load_from_json(
-                util.get_test_resource_filepath('cleaning/pharm_relation_type_constraints_broken1.json'))
-
-    def test_missing_object_constraints_2(self):
-        store = RelationTypeConstraintStore()
-        with self.assertRaises(ValueError):
-            store.load_from_json(
-                util.get_test_resource_filepath('cleaning/pharm_relation_type_constraints_broken2.json'))
-
-    def test_missing_subject_constraints_3(self):
-        store = RelationTypeConstraintStore()
-        with self.assertRaises(ValueError):
-            store.load_from_json(
-                util.get_test_resource_filepath('cleaning/pharm_relation_type_constraints_broken3.json'))
-
-    def test_missing_subject_constraints_4(self):
-        store = RelationTypeConstraintStore()
-        with self.assertRaises(ValueError):
-            store.load_from_json(
-                util.get_test_resource_filepath('cleaning/pharm_relation_type_constraints_broken4.json'))
