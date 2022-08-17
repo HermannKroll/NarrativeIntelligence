7622d840fd2ce108d204209ad0f0788901fe0358
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2022-08-17 07:56:16 +0200

    #137 implemented live service query count retrieval

--- src/narraint/queryengine/verify_query_processing.py
+++ src/narraint/queryengine/verify_query_processing.py
@@ -1,44 +1,54 @@
+import logging
+
 from kgextractiontoolbox.progress import Progress
 from narraint.frontend.ui.views import View
 from narraint.queryengine.engine import QueryEngine
+import requests
 
 
+# inputs = [
+#     ["\"post-acute COVID-19 syndrome\" associated Disease", "PubMed", 1424],
+#     ["Covid19 associated Target", "PubMed", 18039],
+#     ["Simvastatin induces Rhabdomyolysis", "PubMed", 82],
+#     ["Metformin inhibits mtor", "PubMed", 282],
+#     ["Metformin treats \"Diabetes Mellitus\" _AND_ Metformin associated human", "PubMed", 4055]
+# ]
 inputs = [
-    ["\"post-acute COVID-19 syndrome\" associated Disease", 1424],
-    ["Covid19 associated Target", 18039],
-    ["Simvastatin induces Rhabdomyolysis", 82],
-    ["Metformin inhibits mtor", 282],
-    ["Metformin treats \"Diabetes Mellitus\" _AND_ Metformin associated human", 4055]
+    '"post-acute COVID-19 syndrome" associated Disease',
+    'Covid19 associated Target',
+    'Simvastatin induces Rhabdomyolysis',
+    'Metformin inhibits mtor',
+    'Metformin treats "Diabetes Mellitus" _AND_ Metformin associated human',
 ]
+
+data_source = 'PubMed'
 
 
 def main():
-    # DONE It should run some example queries and check that at least a certain number of documents is contained
-    #  (DB might be updated, and so, we should to a greater than comparison to consider updates).
-
-    # DONE For example, select 5 example queries and then process the query, count the number of distinct documents
-    #  and compare it the actual number of publications in our Live service www.narrative.pubpharm.de
-
-    # TODO FOR NOW: store queries as an array with the corresponding result count. Evaluate the query and compare the
-    #  result with the previously stored amount.
-    #  Next step is to retrieve the live count for each query from the live database to be more flexible.
-
-    # progress = 0
-    # p = Progress(total=len(inputs))
-    # p.start_time()
+    j = 0
+    p = Progress(text='Query processing tests')
+    p.start_time()
 
     for i in inputs:
-        graph_query, _ = View.instance().translation.convert_query_text_to_fact_patterns(i[0])
+        # Calculate the local service result count
+        graph_query, _ = View.instance().translation.convert_query_text_to_fact_patterns(i)
+        results = QueryEngine.process_query_with_expansion(graph_query=graph_query, document_collection_filter={'PubMed'})
 
-        # TODO test for more than PubMed collection
-        results = QueryEngine.process_query_with_expansion(graph_query=graph_query, document_collection_filter={"PubMed"})
+        # Retrieve the live service query result
+        res = requests.get(f'http://narrative.pubpharm.de/query',
+                           params={'query': i, 'data_source': data_source})
+        if not 200 <= res.status_code < 300:
+            continue
 
-        assert len(results) >= i[1]
+        # Calculate the live service result count
+        data = res.json()
+        count = data['results']['s']
 
-        # p.print_progress(progress + 1)
-        # progress += 1
-
-    # p.done()
+        # Check that the local query processing retrieves as many results as the live service.
+        if len(results) >= count:
+            j += 1
+    logging.info(f'{j} of {len(inputs)} tests passed')
+    p.done()
 
 
 if __name__ == "__main__":
