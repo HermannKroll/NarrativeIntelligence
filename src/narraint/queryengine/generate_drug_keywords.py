0b9d03850bddcf3dac57a6c91237e67a11e205c0
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2022-08-25 10:51:34 +0200

    #139 entity names are not contained in drug keywords anymore; most plural forms converted to singular

--- src/narraint/queryengine/generate_drug_keywords.py
+++ src/narraint/queryengine/generate_drug_keywords.py
@@ -1,36 +1,89 @@
 import ast
 import random
-import sys
 
 import sqlalchemy
 from typing import List
 
 from sqlalchemy import and_
 from yake import KeywordExtractor
+from nltk.stem.porter import PorterStemmer
 
 from kgextractiontoolbox.backend.models import Document
 from kgextractiontoolbox.progress import Progress
 from narraint.backend.database import SessionExtended
 from narraint.backend.models import PredicationInvertedIndex, TagInvertedIndex, EntityKeywords
+from narrant.entity.entityresolver import EntityResolver
 
-MAX_NGRAM_WORD_SIZE = 3
-NUM_KEYWORDS = 20
+# used to switch between table access and debugging console prints
+ACCESS_ENTITY_KEYWORDS_TABLE = True
+
+MAX_NGRAM_WORD_SIZE = 1
+NUM_KEYWORDS = 25
+
+extractor = KeywordExtractor(n=MAX_NGRAM_WORD_SIZE, top=NUM_KEYWORDS, dedupLim=0.9, dedupFunc="jaro")
+stemmer = PorterStemmer()
 
 
-def generate_keywords(extractor: KeywordExtractor, text: str) -> str:
+def generate_stem_dict(text: str) -> dict[str, str]:
     """
-    Generates a list of keywords and normalizes the score to a value between 1 and 8 for HTML style purposes.
+    Generates a dict containing a pair of the word stem and the shortest word
+    which results the corresponding stem.
 
-    :param extractor: yake.KeywordExtractor
+    :param text: string to evaluate
+    :return: dict with stem - word combination
+    """
+    stem_dict = dict()
+
+    # remove all unwanted chars
+    mapping = text.maketrans('','',',.!?":\'*+')
+    text = text.translate(mapping)
+
+    for word in set(text.split(' ')):
+        temp = stemmer.stem(word)
+        if temp in stem_dict.keys():
+            if len(stem_dict[temp]) > len(word):
+                stem_dict[temp] = word
+            continue
+        stem_dict[temp] = word
+
+    return stem_dict
+
+
+def generate_keywords(text: str, entity_name: str, stem_dict: dict) -> str:
+    """
+    Generates a list of keywords and normalizes the score to a value between 1
+    and 8 for HTML style purposes.
+
     :param text: string from which the keywords are extracted
+    :param entity_name: name of the current entity to ignore it as a key
+    :param stem_dict: dictionary word stem with the shortest stemmed word
     :return: JSON-style list as a string containing 20 keywords
     """
     #  keywords [(ngram: str, score: float)]
-    keywords = extractor.extract_keywords(text)
-
-    return_str = ""
-
+    raw_keywords = extractor.extract_keywords(text)
+    keywords = list()
     try:
+        for i in range(len(raw_keywords)):
+            keyword: str = raw_keywords[i][0]
+            # ignore the key if it is part of the entity_name
+            if entity_name.lower().find(keyword.lower()) >= 0 \
+                    or entity_name.lower() == keyword.lower():
+                continue
+            # replace keywords with the most likely stem
+            stem = stemmer.stem(keyword)
+            if stem in stem_dict.keys():
+                if len(stem) + 1 == stem_dict[stem] \
+                        and stem_dict[stem][-1] == 's':
+                    keywords.append((stem, raw_keywords[i][1]))
+                else:
+                    keywords.append((stem_dict[stem], raw_keywords[i][1]))
+            else:
+                keywords.append(raw_keywords[i])
+
+        # just remove the lowest valued key if no keyword equals the entity name
+        while len(keywords) > 20:
+            keywords.remove(keywords[-1])
+
         #  normalize data for better html visualizations
         minimum = keywords[0][1]
         denominator = keywords[-1][1] - keywords[0][1]  # max - min
@@ -39,33 +92,44 @@ def generate_keywords(extractor: KeywordExtractor, text: str) -> str:
         for obj in keywords:
             #  inverted normalized scores (1-8) for text size
             #  lower value means higher relevance (-> inverted)
-            normalized_keywords.append({obj[0]: 8 - int(((obj[1] - minimum) / denominator) * 7)})
-        return_str = str(normalized_keywords)
+            normalized_keywords.append({obj[0]: 8 - int(((obj[1] - minimum) /
+                                                         denominator) * 7)})
+        keywords = normalized_keywords
 
     finally:
-        return return_str
+        return str(keywords)
 
 
 def main():
     """
-    Generates for each drug (enitity_id having subject_type == "Drug") a JSON list of keywords stored as a string in the
-    corresponding table (drug_keywords). 100 or less random abstracts are concatenated to create a pseudo random
-    information base about each drug.
+    Generates for each drug (entity_id having subject_type == "Drug") a JSON
+    list of keywords stored as a string in the corresponding table
+    (drug_keywords). 100 or less random abstracts are concatenated to create a
+    pseudo random information base about each drug.
 
     :return: None
     """
     entity_type = "Drug"
     document_collection = "PubMed"
 
-    extractor = KeywordExtractor(n=MAX_NGRAM_WORD_SIZE, top=NUM_KEYWORDS)
-
     session = SessionExtended.get()
-    q = session.query(PredicationInvertedIndex.subject_id) \
-        .filter(PredicationInvertedIndex.subject_type == entity_type).distinct()
+
+    if ACCESS_ENTITY_KEYWORDS_TABLE:
+        # remove all previously stored drug keywords
+        q = session.query(EntityKeywords)
+        q = q.filter(EntityKeywords.entity_type == "Drug")
+        q = q.delete()
+        print(f"{q} previously stored DRUG keywords deleted")
+        session.commit()
+
+    # query all existing drug entities
+    q = session.query(PredicationInvertedIndex.subject_id)
+    q = q.filter(PredicationInvertedIndex.subject_type == entity_type)
+    q = q.distinct()
 
     drugs: List[sqlalchemy.engine.row.Row] = q.all()  # first()#
     if drugs:
-        sys.stdout.write(f"Creating keywords for {len(drugs)} drugs\n")
+        print(f"Creating keywords for {len(drugs)} drugs")
     else:
         print("Could not retrieve drug ids. Exiting.")
         return
@@ -74,13 +138,13 @@ def main():
     p.start_time()
 
     skipped_drugs = 0
-    for i in range(len(drugs)):  # range(1):#
+    for i in range(len(drugs)):  #range(100):#
         # retrieve all document_ids for one drug
-        subject_id = dict(drugs[i])["subject_id"]
-        q = session.query(TagInvertedIndex.document_ids) \
-            .filter(TagInvertedIndex.document_collection == document_collection) \
-            .filter(TagInvertedIndex.entity_id == subject_id) \
-            .filter(TagInvertedIndex.entity_type == entity_type)
+        entity_id = dict(drugs[i])["subject_id"]
+        q = session.query(TagInvertedIndex.document_ids)
+        q = q.filter(TagInvertedIndex.document_collection == document_collection)
+        q = q.filter(TagInvertedIndex.entity_id == entity_id)
+        q = q.filter(TagInvertedIndex.entity_type == entity_type)
         doc_ids = q.first()
 
         # choose up to 100 random document_ids out of all available documents to prepare the sample text
@@ -102,15 +166,18 @@ def main():
             text.append(res[0])
 
         text = " ".join([t for t in text])
-
-        keywords = generate_keywords(extractor, text)
-        # print(keywords)
-
-        if keywords == "":
+        entity_name = EntityResolver.instance().get_name_for_var_ent_id(entity_id, entity_type)
+        stem_dict = generate_stem_dict(text)
+        keywords = generate_keywords(text, entity_name, stem_dict)
+        if keywords == "[]":
             skipped_drugs += 1
             continue
 
-        EntityKeywords.insert_entity_keyword_data(session, subject_id, entity_type, keywords)
+        if ACCESS_ENTITY_KEYWORDS_TABLE:
+            EntityKeywords.insert_entity_keyword_data(session, entity_id, entity_type, str(keywords))
+        else:
+            print("\n", i, entity_name, keywords)
+
         p.print_progress(i + 1)
 
     session.remove()
