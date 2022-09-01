132001ea98092056961157950f476b61eedc8062
HermannKroll <hermann.kroll@gmail.com>
2022-09-01 21:52:50 +0200

    speed up log computation + add try catch for robustness

--- src/narraint/queryengine/log_statistics.py
+++ src/narraint/queryengine/log_statistics.py
@@ -15,18 +15,24 @@ def get_date_of_today():
 def get_json_of_log(path):
     data = []
     for filename in os.listdir(path):
-        with open(os.path.join(path, filename), 'r') as f:
-            headers = f.readline().rstrip()
-            header_list = headers.split('\t')
-            for line in f.readlines():
-                details = line.split('\t')
-                details = [x.strip() for x in details]
-                structure = {key: value for key, value in zip(header_list, details)}
-                data.append(structure)
-    data_json = json.dumps(data, indent=4)
-    #print(data_json)
-    json_object = json.loads(data_json)
-    return json_object
+        try:
+            with open(os.path.join(path, filename), 'r') as f:
+                headers = f.readline().rstrip()
+                header_list = headers.split('\t')
+                for line in f.readlines():
+                    line = line.lower()
+                    details = line.split('\t')
+                    details = [x.strip() for x in details]
+                    structure = {key: value for key, value in zip(header_list, details)}
+                    date = structure['timestamp'].split('-')[0]
+                    structure["data_object"] = datetime.strptime(date, '%Y.%m.%d').date()
+                    data.append(structure)
+        except:
+            pass
+    # data_json = json.dumps(data, indent=4)
+    # print(data_json)
+    # json_object = json.loads(data_json)
+    return data
 
 
 def get_list_of_parameter(json_object, parameter):
@@ -36,7 +42,7 @@ def get_list_of_parameter(json_object, parameter):
         if i.__contains__(parameter) and not i[parameter] in parameter_list:
             parameter_list.append(i[parameter])
     parameter_list = sorted(parameter_list)
-    #print(parameter_list)
+    # print(parameter_list)
     return parameter_list
 
 
@@ -66,8 +72,7 @@ def get_most_searched_parameter_per_time(amount, json_object, parameter, time):
     for i in json_object:
         if i.__contains__(parameter):
             if i.__contains__('timestamp') and i['timestamp'] != '':
-                date = i['timestamp'].split('-')[0]
-                date_object = datetime.strptime(date, '%Y.%m.%d').date()
+                date_object = i["data_object"]
                 if time == "t":
                     if date_object == today:
                         amount_list[parameter_list.index(i[parameter])] += 1
@@ -184,8 +189,11 @@ def create_dictionary_of_logs():
     result = dict()
     result_narrative = dict()
     result_overview = dict()
-    result_narrative['topTenQueries'], result_overview['topTenQueries'] = get_most_searched_parameter(100, narrative_json, overview_json)
-    result_narrative['amountQueries'], result_overview['amountQueries'] = get_all_amounts_of_occurrences(narrative_json, overview_json)
+    result_narrative['topTenQueries'], result_overview['topTenQueries'] = get_most_searched_parameter(100,
+                                                                                                      narrative_json,
+                                                                                                      overview_json)
+    result_narrative['amountQueries'], result_overview['amountQueries'] = get_all_amounts_of_occurrences(narrative_json,
+                                                                                                         overview_json)
     result_narrative['graphInput'], result_overview['graphInput'] = get_graph_input(narrative_json, overview_json)
     result['narrative'] = result_narrative
     result['overview'] = result_overview
@@ -194,16 +202,16 @@ def create_dictionary_of_logs():
 
 
 def main():
-    #print(get_json_of_log(narrative_path))
-    #query_json = get_json_of_log(narrative_path)
-    #print(get_amount_of_queries(query_json, "t"))
+    # print(get_json_of_log(narrative_path))
+    # query_json = get_json_of_log(narrative_path)
+    # print(get_amount_of_queries(query_json, "t"))
     # top_queries = get_most_searched_queries(5, query_json, "query string")
     # print(top_queries)
     # print('Amount of queries performed in the last week: {}'.format(get_amount_of_queries(query_json, "w")))
     # print('Amount of queries performed in the last month: {}'.format(get_amount_of_queries(query_json, "m")))
-    #print(create_dictionary_of_logs())
-   # print(get_graph_input(query_json))
-   get_json_of_log(overview_path)
+    # print(create_dictionary_of_logs())
+    # print(get_graph_input(query_json))
+    get_json_of_log(overview_path)
 
 
 if __name__ == "__main__":
