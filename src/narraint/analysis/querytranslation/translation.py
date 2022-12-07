68410870855be9c7d7327f01477b90c2e9c63d85
HermannKroll <hermann.kroll@gmail.com>
2022-12-07 12:17:46 +0100

    #179 map entity types + predicates + simple entity type resolution

--- src/narraint/analysis/querytranslation/translation.py
+++ src/narraint/analysis/querytranslation/translation.py
@@ -1,8 +1,11 @@
 import logging
+from copy import copy
+from typing import Set
 
 from narraint.frontend.entity.entitytagger import EntityTagger
 from narraint.frontend.entity.query_translation import QueryTranslation
 from narraint.queryengine.query import GraphQuery
+from narrant.entity.entity import Entity
 
 QUERY_1 = "Metformin Diabetes"
 QUERY_2 = "Metformin treats Diabetes"
@@ -24,27 +27,37 @@ class QueryTranslationToGraph:
     def __load_schema_graph(self):
         translation = QueryTranslation()
         self.entity_types = translation.variable_type_mappings
-        self.allowed_predicates = translation.allowed_predicates
+        self.allowed_predicates = {k: k for k in translation.allowed_predicates}
         self.max_spaces_in_entity_types = max([len(t.split(' ')) - 1 for t in self.entity_types])
         logging.info(f'Longest entity type has {self.max_spaces_in_entity_types} spaces')
 
-    def __greedy_find_entity_types_in_keyword_query(self, keyword_query):
-        keywords = keyword_query.split(' ')
-        term2entitytypes = list()
+    def __greedy_find_dict_entries_in_keywords(self, keywords, lookup_dict):
+        term2dictentries = list()
         for i in range(self.max_spaces_in_entity_types, 0, -1):
             for j in range(len(keywords)):
                 combined_word = ' '.join([k for k in keywords[j:j + i]])
-                if combined_word in self.entity_types:
-                    term2entitytypes.append((combined_word, self.entity_types[combined_word]))
+                if combined_word in lookup_dict:
+                    term2dictentries.append((combined_word, lookup_dict[combined_word]))
+        return term2dictentries
 
-        logging.debug('Term2EntityType mapping: ')
+    def __greedy_find_predicates_in_keywords(self, keywords):
+        term2predicates = self.__greedy_find_dict_entries_in_keywords(keywords, self.allowed_predicates)
+        logging.info('Term2predicate mapping: ')
+        for k, v in term2predicates:
+            logging.info(f'    {k} -> {v}')
+        return term2predicates
+
+    def __greedy_find_entity_types_in_keywords(self, keywords):
+        term2entitytypes = self.__greedy_find_dict_entries_in_keywords(keywords, self.entity_types)
+        logging.info('Term2EntityType mapping: ')
         for k, v in term2entitytypes:
-            logging.debug(f'    {k} -> {v}')
+            logging.info(f'    {k} -> {v}')
+        return term2entitytypes
 
-    def __greedy_find_entities_in_keyword_query(self, keyword_query):
+    def __greedy_find_entities_in_keywords(self, keywords):
         logging.debug('--' * 60)
         logging.debug('--' * 60)
-        keywords_remaining = keyword_query.split(' ')
+        keywords_remaining = copy(keywords)
         keywords_not_mapped = list()
         term2entities = list()
         while keywords_remaining:
@@ -76,24 +89,50 @@ class QueryTranslationToGraph:
         terms_mapped = ' '.join([t[0] for t in term2entities])
         logging.debug(f'Found entities in part: {terms_mapped}')
         logging.debug(f'Cannot find entities in part: {keywords_not_mapped}')
-        logging.debug('Term2Entity mapping: ')
+        logging.info('Term2Entity mapping: ')
         for k, v in term2entities:
-            logging.debug(f'    {k} -> {v}')
+            logging.info(f'    {k} -> {v}')
 
         logging.debug('--' * 60)
         logging.debug('--' * 60)
+        return term2entities
+
+    def __unify_entity_types_in_entity_set(self, entity_set: Set[Entity]):
+        e_types = list(set([e.entity_type for e in entity_set]))
+        # only a single entity type - easy
+        if len(e_types) == 1:
+            return e_types
+        # What if the same entity id refers to multiple types? Can we resolve it?
+        type2id = {}
+        for t in e_types:
+            type2id[t] = {e.entity_id for e in entity_set if e.entity_type == t}
+
+        # If the length of entity ids is equal between all types, the entities must share the same type
+        # In that case both entity types are ok
+        if False not in [type2id[e_types[0]] == type2id[e_types[i]] for i in range(1, len(e_types))]:
+            # They agree easy
+            return e_types
+
+        # They do not agree
+        # Return the entity type that has the most found entities
+        return list([sorted([(t, len(v)) for t, v in type2id.items()], reverse=True, key=lambda x: x[1])[0][0]])
 
     def translate_keyword_query(self, keyword_query) -> GraphQuery:
         keyword_query = keyword_query.lower().strip()
-        self.__greedy_find_entity_types_in_keyword_query(keyword_query)
-        self.__greedy_find_entities_in_keyword_query(keyword_query)
+        keywords = keyword_query.split(' ')
+        term2predicates = self.__greedy_find_predicates_in_keywords(keywords)
+        term2entitytypes = self.__greedy_find_entity_types_in_keywords(keywords)
+        term2entities = self.__greedy_find_entities_in_keywords(keywords)
+        logging.info('Unified Term2EntityType mapping: ')
+        for k, v in term2entities:
+            logging.info(f'    {k} -> {self.__unify_entity_types_in_entity_set(v)}')
         return GraphQuery()
 
 
 def main():
     logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                         datefmt='%Y-%m-%d:%H:%M:%S',
-                        level=logging.DEBUG)
+                        level=logging.INFO)
 
     trans = QueryTranslationToGraph()
     for q in QUERIES:
