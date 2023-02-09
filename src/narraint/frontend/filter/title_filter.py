3943395a77193e21780b6b5336f85abad52e9959
HermannKroll <hermann.kroll@gmail.com>
2023-02-09 10:28:36 +0100

    search with word fragments by default

--- src/narraint/frontend/filter/title_filter.py
+++ src/narraint/frontend/filter/title_filter.py
@@ -10,7 +10,9 @@ class TitleFilter:
         if not search_str or not search_str.strip():
             return results
         else:
-            eldar_q = Query(search_str)
+            # match word will have the same effect
+            search_str = search_str.replace('*', '')
+            eldar_q = Query(search_str, match_word=False)
             return list([r for r in results if eldar_q(r.title)])
 
 
