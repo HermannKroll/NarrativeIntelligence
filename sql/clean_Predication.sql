7537af451a6177e53741435796fce6f6a9cfa132
HermannKroll <hermann.kroll@gmail.com>
2023-02-10 08:08:04 +0100

    handled by pharmaceutical rules

--- sql/clean_Predication.sql
+++ sql/clean_Predication.sql
@@ -3,11 +3,11 @@ DELETE FROM public.Predication AS p
 WHERE (p.subject_id = 'newentry' and p.subject_type = 'Gene') OR (p.object_id = 'newentry' and p.object_type = 'Gene');
 
 -- Update Dosage Form
-UPDATE public.Predication SET relation = 'administered' WHERE subject_type = 'DosageForm' or object_type = 'DosageForm';
+-- UPDATE public.Predication SET relation = 'administered' WHERE subject_type = 'DosageForm' or object_type = 'DosageForm';
 
 -- Update Method and LabMethod relation
-UPDATE public.Predication SET relation = 'method' WHERE subject_type = 'Method' or object_type = 'Method';
-UPDATE public.Predication SET relation = 'method' WHERE subject_type = 'LabMethod' or object_type = 'LabMethod';
+-- UPDATE public.Predication SET relation = 'method' WHERE subject_type = 'Method' or object_type = 'Method';
+-- UPDATE public.Predication SET relation = 'method' WHERE subject_type = 'LabMethod' or object_type = 'LabMethod';
 
 -- Delete all symmetric predications (subject = object)
 DELETE FROM public.Predication AS p WHERE p.subject_id = p.object_id and p.subject_type = p.object_type;
