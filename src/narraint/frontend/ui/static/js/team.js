daae305282fb469eb99c3e431e0e3a5d5f226156
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2023-03-29 11:52:10 +0200

    Removed dynamic text visibility for each member of the /team page

--- src/narraint/frontend/ui/static/js/team.js
+++
@@ -1,8 +0,0 @@
-$("div.img_container").hover(function(){
-  console.log("hover");
-  $(this).children("div.overlay").css("height", $("div.text").outerHeight());
-  //$(this).children("div.overlay").children("div.text").css("width", $("div.overlay").outerWidth());
-  console.log($("div.overlay").outerHeight())
-  }, function(){
-    $(this).children("div.overlay").css("height", "0");
-});
