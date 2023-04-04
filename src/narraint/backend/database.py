4d4d17fabc80ac6408f28369da4fdf06b97b6943
HermannKroll <hermann.kroll@gmail.com>
2023-04-04 08:22:57 +0200

    critical bugfix: session extended was not created if an session of the toolbox was there before

--- src/narraint/backend/database.py
+++ src/narraint/backend/database.py
@@ -8,11 +8,13 @@ from kgextractiontoolbox.backend.database import Session
 class SessionExtended(Session):
     is_sqlite = False
     is_postgres = False
+    _instance_extended = None
 
     @classmethod
-    def get(cls, connection_config: str = BACKEND_CONFIG, declarative_base=Extended) -> ScopedSession:
-        if not cls._instance:
-            cls._instance = Session.get(connection_config, declarative_base)
+    def get(cls, connection_config: str = BACKEND_CONFIG, declarative_base=Extended,
+            force_create=False) -> ScopedSession:
+        if not SessionExtended._instance_extended:
+            SessionExtended._instance_extended = Session.get(connection_config, declarative_base, force_create=True)
             SessionExtended.is_postgres = cls._instance.is_postgres
             SessionExtended.is_sqlite = cls._instance.is_sqlite
-        return cls._instance
+        return SessionExtended._instance_extended
