afe7a15bb6bb9ce714b2270abeeca8896f13aa04
HermannKroll <hermann.kroll@gmail.com>
2023-10-11 17:02:49 +0200

    remove old config stuff

--- src/narraint/config.py
+++ src/narraint/config.py
@@ -24,13 +24,6 @@ if not os.path.isdir(TMP_DIR):
 if not os.path.isdir(TMP_DIR_TAGGER):
     os.makedirs(TMP_DIR_TAGGER)
 
-# UMLS
-UMLS_DATA = os.path.join(DATA_DIR, "umls/MRCONSO.RRF.gz")
-UMLS_MAPPING = os.path.join(DATA_DIR, "umls/mapping.json")
-
-# SemMed
-SEMMEDDB_CONFIG = os.path.join(CONFIG_DIR, 'semmed.json')
-
 # Backend for Tagging
 BACKEND_CONFIG = os.path.join(CONFIG_DIR, "backend.json")
 
@@ -45,9 +38,6 @@ ENTITY_TAGGING_INDEX = os.path.join(TMP_DIR, 'entity_tagging_index.pkl')
 
 # Autocompletion Index
 AUTOCOMPLETION_TMP_INDEX = os.path.join(TMP_DIR, 'autocompletion.pkl')
-
-# NLP Config
-NLP_CONFIG = os.path.join(CONFIG_DIR, 'nlp.json')
 
 # Drug keyword extraction stopword list
 DRUG_KEYWORD_STOPWORD_LIST = os.path.join(RESOURCE_DIR, 'stopwords_drug_keywords.txt')
