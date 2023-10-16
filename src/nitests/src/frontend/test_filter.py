f550c2b1aa65b10630bcdc6825cbd860a6f8cfd5
HermannKroll <hermann.kroll@gmail.com>
2023-10-16 13:38:52 +0200

    add more tests

--- src/nitests/src/frontend/test_filter.py
+++ src/nitests/src/frontend/test_filter.py
@@ -36,6 +36,8 @@ class FilterTestCase(TestCase):
         self.assertEqual(1, len(TitleFilter.filter_documents([dsys], "systemat* revi*")))
         self.assertEqual(1, len(TitleFilter.filter_documents([dsys], "systematic review")))
         self.assertEqual(0, len(TitleFilter.filter_documents([dsys], "systematic+review")))
+        self.assertEqual(1, len(TitleFilter.filter_documents([dsys], "systemat or review")))
+        self.assertEqual(1, len(TitleFilter.filter_documents([dsys], "systemat and review")))
 
     def test_time_filter(self):
         self.assertEqual(5, len(TimeFilter.filter_documents_by_year(self.results, year_start=2018, year_end=0)))
