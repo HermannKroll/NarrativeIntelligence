ca0d61d554c11a4cdf2cb6dfe1d1114fadb4576a
HermannKroll <hermann.kroll@gmail.com>
2024-01-30 07:45:34 +0100

    rework script so that exit code is considered

--- scripts/update_every_month.sh
+++ scripts/update_every_month.sh
@@ -2,3 +2,7 @@
 
 # Update clinical trial phases for drug overviews
 python3 ~/NarrativeIntelligence/src/narraint/clinicaltrials/extract_trial_phases.py
+if [[ $? != 0 ]]; then
+    echo "Previous script returned exit code != 0 -> Stopping pipeline."
+    exit -1
+fi
