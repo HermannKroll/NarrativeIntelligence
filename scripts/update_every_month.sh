ee6646b9a8226cbb1dd1006ead64ad195340419d
HermannKroll <hermann.kroll@gmail.com>
2024-03-11 10:27:19 +0100

    move clinical trial processing into default pipeline

--- scripts/update_every_month.sh
+++ scripts/update_every_month.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 
-# Update clinical trial phases for drug overviews
-python3 ~/NarrativeIntelligence/src/narraint/clinicaltrials/extract_trial_phases.py
-if [[ $? != 0 ]]; then
-    echo "Previous script returned exit code != 0 -> Stopping pipeline."
-    exit -1
-fi
