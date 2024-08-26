085589df52e41fce8a984de6c678bfb113527a74
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2024-08-26 11:10:55 +0200

    #287 add caching for data sources; add db collection method

--- src/narraint/frontend/filter/data_sources_filter.py
+++ src/narraint/frontend/filter/data_sources_filter.py
@@ -6,12 +6,21 @@ from narraint.config import RESOURCE_DIR
 
 class DataSourcesFilter:
     file_path = os.path.join(RESOURCE_DIR, "data_sources.json")
+    data_sources = None
 
     @staticmethod
     def get_available_data_sources():
+        if DataSourcesFilter.data_sources is not None:
+            return DataSourcesFilter.data_sources
+
         if not os.path.exists(DataSourcesFilter.file_path):
             raise FileNotFoundError(f"Config file not found: {DataSourcesFilter.file_path}")
 
         with open(DataSourcesFilter.file_path) as f:
-            doc_collections = json.load(f)
-        return doc_collections
+            DataSourcesFilter.data_sources = json.load(f)
+        return DataSourcesFilter.data_sources
+
+    @staticmethod
+    def get_available_db_collections():
+        doc_collections = DataSourcesFilter.get_available_data_sources()
+        return {c["collection"] for c in doc_collections}
