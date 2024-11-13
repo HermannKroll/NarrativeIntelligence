2b86ee9b0d055483812337f6530182f523ddc211
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2024-11-13 11:02:20 +0100

    #294 renamed predication_inverted_index column to document_ids

--- src/narraint/backend/models.py
+++ src/narraint/backend/models.py
@@ -52,7 +52,12 @@ class PredicationInvertedIndex(Extended, DatabaseTable):
     object_id = Column(String, nullable=False, index=True)
     object_type = Column(String, nullable=False, index=True)
     support = Column(Integer, nullable=False)
-    provenance_mapping = Column(String, nullable=False)
+    document_ids = Column(String, nullable=False)
+
+    @staticmethod
+    def prepare_document_ids(document_ids_str: str):
+        return list(int(doc_id) for doc_id in document_ids_str.strip("[]").split(","))
+
 
 
 class Tagger(models.Tagger):
