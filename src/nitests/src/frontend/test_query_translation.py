d93b5da7342f9de9e5e9519cc0facb29febd5f84
Pascal Sackhoff <p.sackhoff@tu-bs.de>
2025-02-13 14:46:00 +0100

    Fix query translation test for changed entity tagger impl

--- src/nitests/src/frontend/test_query_translation.py
+++ src/nitests/src/frontend/test_query_translation.py
@@ -1,10 +1,41 @@
 from unittest import TestCase
 
+from sqlalchemy import delete, insert
+
+from narraint.backend.database import SessionExtended
+from narraint.backend.models import IndexVersion, EntityTaggerData
+from narraint.frontend.entity.entitytagger import EntityTagger
 from narraint.frontend.entity.query_translation import QueryTranslation
 from narrant.entitylinking.enttypes import PLANT_FAMILY_GENUS
 
+tagger_entries = {
+    ('MESH:D007267', 'DosageForm', None, ' injection'),
+    ('CHEMBL1064', 'Drug', None, ' simvastatin'),
+    ('CHEMBL1431', 'Drug', None, ' metformin'),
+    ('9606', 'Species', None, ' human'),
+    ('MESH:D003920', 'Disease', None, ' diabetes mellitus')
+}
+
 
 class QueryTranslationTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        session = SessionExtended.get()
+
+        session.execute(delete(IndexVersion).where(IndexVersion.name == EntityTagger.NAME))
+        session.execute(insert(IndexVersion).values(name=EntityTagger.NAME, version=EntityTagger.VERSION))
+
+        session.commit()
+
+        entity_tagger_data = list()
+        for ent_id, ent_type, ent_class, synonyms in tagger_entries:
+            entity_tagger_data.append(dict(entity_id=ent_id,
+                                           entity_type=ent_type,
+                                           entity_class=ent_class,
+                                           synonym=synonyms,
+                                           synonym_processed=synonyms))
+
+        EntityTaggerData.bulk_insert_values_into_table(session, entity_tagger_data)
 
     def setUp(self) -> None:
         self.translation = QueryTranslation()
@@ -39,7 +70,7 @@ class QueryTranslationTestCase(TestCase):
 
         graph_query, text = self.translation.convert_json_to_fact_patterns(json)
 
-        self.assertIsNotNone(graph_query)
+        self.assertIsNotNone(graph_query, msg=text)
         self.assertTrue(graph_query.has_entities())
         self.assertEqual(len(graph_query.to_dict()["fact_patterns"]), 2)
 
@@ -106,3 +137,34 @@ class QueryTranslationTestCase(TestCase):
                 }'''
         graph_query, text = self.translation.convert_json_to_fact_patterns(json)
         self.assertIsNone(graph_query)
+
+
+def generate_test_data():
+    from narraint.frontend.entity.entitytagger import EntityTagger
+
+    import tqdm
+
+    tagger_data = set()
+    entity_tagger = EntityTagger()
+
+    terms_to_explain = {
+        "metformin",
+        "human",
+        "diabetes mellitus",
+        "simvastatin",
+        "injection"
+    }
+
+    known_terms = set()
+    for term in tqdm.tqdm(terms_to_explain):
+        for entity in entity_tagger.tag_entity(term):
+            if entity.entity_name not in terms_to_explain or entity.entity_name in known_terms:
+                continue
+            known_terms.add(entity.entity_name)
+            tagger_data.add((entity.entity_id, entity.entity_type, entity.entity_class, " " + entity.entity_name))
+
+    print("tagger_entries = {\n" + ",\n".join("\t" + str(t) for t in list(tagger_data)) + "\n}")
+
+
+if __name__ == "__main__":
+    generate_test_data()
