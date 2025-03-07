2d82a017783ef1ee9cd71e5c827be24768735614
HermannKroll <hermann.kroll@gmail.com>
2025-03-07 08:50:17 +0100

    remove pre-initialization as that may cause issues when workers are forking the DB connection

--- src/narraint/frontend/ui/apps.py
+++ src/narraint/frontend/ui/apps.py
@@ -6,30 +6,19 @@ import threading
 
 from django.apps import AppConfig
 
-from narraint.frontend.entity.entitytagger import EntityTagger
-from narraint.ranking.corpus import DocumentCorpus
-from narrant.entity.entityresolver import EntityResolver
 from narraint.logging_config import configure_logging
+
 
 def log_output(pipe, level):
     for line in iter(pipe.readline, ''):
         logging.log(level, line.strip())
 
+
 class UiConfig(AppConfig):
     name = 'ui'
-    resolver = None
-    entity_tagger = None
 
     def ready(self):
         configure_logging()
-        # the following three classes are singleton implementations, so loading them before the worker spawn
-        # is a good idea
-        logging.info('Initializing entity tagger & entity resolver once...')
-        UiConfig.resolver = EntityResolver()
-        UiConfig.entity_tagger = EntityTagger()
-        logging.info('Initializing document corpus once...')
-        UiConfig.corpus = DocumentCorpus()
-        logging.info('Index loaded')
 
         if len(sys.argv) > 1 and sys.argv[1] in ['collectstatic', 'migrate']:
             logging.info('Skipping dailyWorker initialization')
