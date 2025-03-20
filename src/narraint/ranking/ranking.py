7159e92f3c49cc05a85ec54279ac14b610061d9d
HermannKroll <hermann.kroll@gmail.com>
2025-03-20 15:46:55 +0100

    #313 start ranking implementation

--- src/narraint/ranking/ranking.py
+++ src/narraint/ranking/ranking.py
@@ -1,14 +1,50 @@
 from typing import List
 
+from narraint.backend.database import SessionExtended
 from narraint.queryengine.result import QueryDocumentResult
 from narraint.ranking.corpus import DocumentCorpus
+from narraint.ranking.indexed_document import retrieve_indexed_documents_from_database_small
+from narraint.ranking.query import AnalyzedQuery
+from narraint.ranking.rankers.ranker_confidence import ConfidenceDocumentRanker
+from narraint.ranking.rankers.ranker_coverage import CoverageDocumentRanker
+from narraint.ranking.rankers.ranker_relational_sim import RelationalSimDocumentRanker
+from narraint.ranking.rankers.ranker_tf_idf_min import TfIdfMinDocumentRanker
 
 
 class GraphRank:
 
     def __init__(self):
         self.corpus = DocumentCorpus()
+        self.rankers = [ConfidenceDocumentRanker(self.corpus), CoverageDocumentRanker(self.corpus),
+                        RelationalSimDocumentRanker(self.corpus), TfIdfMinDocumentRanker(self.corpus)]
+        self.weights = [0.25, 0.25, 0.25, 0.25]
+
+        assert len(self.weights) == len(self.rankers)
+        assert sum(self.weights) == 1.0
 
     def rank_document(self, query_results: List[QueryDocumentResult]) -> List[QueryDocumentResult]:
-        collections = set([r.document_collection for r in query_results])
-        # we need to rank over each collection speparatly
+        session = SessionExtended.get()
+        # we need to retrieve data for each collection separately
+        collections = {r.document_collection for r in query_results}
+        indexed_docs = []
+        for collection in collections:
+            dids = {r.document_id for r in query_results if r.document_collection == collection}
+            indexed_docs.extend(retrieve_indexed_documents_from_database_small(session, document_ids=dids,
+                                                                               document_collection=collection))
+
+        # todo compute fragments
+        fragments = []
+
+        # analyze query
+        # TODO: implement
+        query = AnalyzedQuery()
+
+        # next compute the document score
+        ranker2scores = {}
+        for ranker in self.rankers:
+            ranker2scores[ranker.name] = ranker.rank_documents(query, indexed_docs, fragments)
+
+        doc2scores = {d: 0.0 for d in indexed_docs}
+
+        # remove opened session
+        session.remove()
