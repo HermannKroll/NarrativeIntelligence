81fc2af72b86399a9f74f60adec87f74b970360e
HermannKroll <hermann.kroll@gmail.com>
2025-03-21 10:28:05 +0100

    remove not used dict (it's available in scoring.py)

--- src/narraint/ranking/corpus.py
+++ src/narraint/ranking/corpus.py
@@ -8,19 +8,6 @@ from narraint.backend.database import SessionExtended
 from narraint.backend.models import TagInvertedIndex, ContentData
 from narraint.ranking.indexed_document import ScoredDocumentEntity
 
-PREDICATE_TO_SCORE = {
-    "associated": 0.25,
-    "administered": 1.0,
-    "compares": 1.0,
-    "decreases": 0.5,
-    "induces": 1.0,
-    "interacts": 0.5,
-    "inhibits": 1.0,
-    "metabolises": 1.0,
-    "treats": 1.0,
-    "method": 1.0
-}
-
 
 class DocumentCorpus:
     """
