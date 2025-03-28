9f9a807ab526abef2e43ad5895de90c866a7e60a
HermannKroll <hermann.kroll@gmail.com>
2025-03-28 12:28:30 +0100

    #313 analyze graph queries (create a dict of ent2score mappings)

--- src/narraint/ranking/query.py
+++ src/narraint/ranking/query.py
@@ -1,6 +1,22 @@
+import itertools
+
+from narraint.entity.entity import TranslatedEntity
 from narraint.queryengine.query import GraphQuery
 
 
 class AnalyzedQuery(GraphQuery):
 
-    pass
+    def __init__(self, query: GraphQuery):
+        super().__init__(fact_patterns=query.fact_patterns)
+
+        self.entity2score = {}
+        for fp in self.fact_patterns:
+            for ent in itertools.chain(fp.subjects, fp.objects):
+                if not isinstance(ent, TranslatedEntity):
+                    raise TypeError(f"Expected TranslatedEntity, got {type(ent)}")
+
+                ent_key = ent.get_unique_key()
+                if ent_key not in self.entity2score:
+                    self.entity2score[ent_key] = ent.translation_score
+                else:
+                    self.entity2score[ent_key] = max(ent.translation_score, self.entity2score[ent_key])
