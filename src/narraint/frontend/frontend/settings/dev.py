260acd0aaa7ec2a3f83af6dbf4a66b562036aa78
HermannKroll <hermann.kroll@gmail.com>
2025-04-14 15:26:17 +0200

    #318 use relevant import again

--- src/narraint/frontend/frontend/settings/dev.py
+++ src/narraint/frontend/frontend/settings/dev.py
@@ -1,3 +1,7 @@
+# enforce relevant import
+from .base import *
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4o5h94sf8#+e_$a7*szdfcyf!uhf2er)0kg#qp3r*nac$%pp6q'
 
